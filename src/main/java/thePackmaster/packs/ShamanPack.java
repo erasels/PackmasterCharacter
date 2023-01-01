ad14118baf362f0d1e9bd6bf99b7f4afbb3d62b2
modargo <modargo@gmail.com>
2023-01-01 17:33:55 -0500

    Shaman pack: initial files

---
+++ src/main/java/thePackmaster/packs/ShamanPack.java
@@ -0,0 +1,26 @@
+package thePackmaster.packs;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.cards.downfallpack.*;
+
+import java.util.ArrayList;
+
+public class ShamanPack extends AbstractCardPack {
+    public static final String ID = SpireAnniversary5Mod.makeID("ShamanPack");
+    private static final UIStrings UI_STRINGS = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String NAME = UI_STRINGS.TEXT[0];
+    public static final String DESC = UI_STRINGS.TEXT[1];
+    public static final String AUTHOR = UI_STRINGS.TEXT[2];
+
+    public ShamanPack() {
+        super(ID, NAME, DESC, AUTHOR);
+    }
+
+    @Override
+    public ArrayList<String> getCards() {
+        ArrayList<String> cards = new ArrayList<>();
+        return cards;
+    }
+}
