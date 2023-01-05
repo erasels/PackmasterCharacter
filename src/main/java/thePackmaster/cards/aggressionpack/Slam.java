ae08829e06c871c846a5815a718552c2433a1c3b
modargo <modargo@gmail.com>
2023-01-04 19:24:03 -0500

    Aggression pack: add Slam

--- src/main/java/thePackmaster/cards/aggressionpack/Slam.java
+++ src/main/java/thePackmaster/cards/aggressionpack/Slam.java
@@ -1,43 +1,35 @@
 package thePackmaster.cards.aggressionpack;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.watcher.WallopAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.RagePower;
 import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.cards.AbstractPackmasterCard;
 
-public class Vindicate extends AbstractPackmasterCard {
+public class Slam extends AbstractPackmasterCard {
     public static final String ID = SpireAnniversary5Mod.makeID("Vindicate");
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_DAMAGE = 1;
-    private static final int RAGE = 4;
-    private static final int UPGRADE_RAGE = 2;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_DAMAGE = 2;
 
-    public Vindicate() {
-        super(ID, COST, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+    public Slam() {
+        super(ID, COST, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         this.baseDamage = DAMAGE;
-        this.magicNumber = this.baseMagicNumber = RAGE;
     }
 
     @Override
     public void upp() {
         this.upgradeDamage(UPGRADE_DAMAGE);
-        this.upgradeMagicNumber(UPGRADE_RAGE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
-        this.addToBot(new ApplyPowerAction(p, p, new RagePower(p, this.magicNumber)));
+        this.addToBot(new WallopAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
     }
 }
