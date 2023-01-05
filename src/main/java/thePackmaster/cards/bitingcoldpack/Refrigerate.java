2e28ff0be856826f225ca8fc6395de1a7eb5bda6
ActuallyEnbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2023-01-05 01:15:24 +0000

    Added Biting Cold cardbacks

--- src/main/java/thePackmaster/cards/bitingcoldpack/Refrigerate.java
+++ src/main/java/thePackmaster/cards/bitingcoldpack/Refrigerate.java
@@ -2,13 +2,12 @@ package thePackmaster.cards.bitingcoldpack;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.powers.bitingcoldpack.FrostbitePower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.applyToEnemy;
 
-public class Refrigerate extends AbstractPackmasterCard {
+public class Refrigerate extends BitingColdCard {
     public final static String ID = makeID("Refrigerate");
 
     public Refrigerate() {
