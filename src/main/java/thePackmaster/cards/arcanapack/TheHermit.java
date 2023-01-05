aad224a2f0687dd929ea411d5e19a0c2a4d62330
Alchyr <isithalchyr@gmail.com>
2023-01-05 07:24:41 -0800

    make these actions specific because I Don't Like How The Cards Go Back to the Hand First

--- src/main/java/thePackmaster/cards/arcanapack/TheHermit.java
+++ src/main/java/thePackmaster/cards/arcanapack/TheHermit.java
@@ -1,18 +1,19 @@
 package thePackmaster.cards.arcanapack;
 
-import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
-import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
-import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.patches.arcanapack.AnimatedCardsPatch;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
-import static thePackmaster.util.Wiz.*;
+import static thePackmaster.util.Wiz.atb;
+import static thePackmaster.util.Wiz.att;
 
 public class TheHermit extends AbstractAstrologerCard {
     public final static String ID = makeID("TheHermit");
@@ -26,17 +27,55 @@ public class TheHermit extends AbstractAstrologerCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new SelectCardsInHandAction(1, DiscardAction.TEXT[0], (c)->true,
-                (cards)->{
-                    for (AbstractCard c : cards) {
-                        atb(new DiscardSpecificCardAction(c));
+        atb(new AbstractGameAction() {
+            boolean first = true;
+            @Override
+            public void update() {
+                if (first) {
+                    first = false;
+
+                    if (AbstractDungeon.getMonsters().areMonstersBasicallyDead() || p.hand.isEmpty()) {
+                        this.isDone = true;
+                        return;
                     }
-                    for (AbstractCard c : cards) {
-                        int amt = c.costForTurn == -1 ? EnergyPanel.getCurrentEnergy() : c.costForTurn;
-                        if (amt >= 1)
-                            atb(new DrawCardAction(amt));
+
+                    if (p.hand.size() == 1) {
+                        AbstractCard c = p.hand.getTopCard();
+                        p.hand.moveToDiscardPile(c);
+                        c.triggerOnManualDiscard();
+                        GameActionManager.incrementDiscard(false);
+                        drawFromCard(c);
+
+                        AbstractDungeon.player.hand.applyPowers();
+                        this.isDone = true;
+                        return;
                     }
-                }));
+
+                    AbstractDungeon.handCardSelectScreen.open(DiscardAction.TEXT[0], 1, false);
+                    AbstractDungeon.player.hand.applyPowers();
+                }
+                else {
+                    if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                        for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                            p.hand.moveToDiscardPile(c);
+                            c.triggerOnManualDiscard();
+                            GameActionManager.incrementDiscard(false);
+                            drawFromCard(c);
+                        }
+
+                        AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                        AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+                    }
+
+                    this.isDone = true;
+                }
+            }
+        });
+    }
+    private void drawFromCard(AbstractCard c) {
+        int amt = c.costForTurn == -1 ? EnergyPanel.getCurrentEnergy() : c.costForTurn;
+        if (amt >= 1)
+            att(new DrawCardAction(amt));
     }
 
     public void upp() {
