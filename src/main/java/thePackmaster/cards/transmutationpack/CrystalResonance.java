6bc09c36ae3cd22fd668ccff989aafd67e44ed49
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 09:57:52 -0800

    implement crystal resonance

--- src/main/java/thePackmaster/cards/transmutationpack/CrystalResonance.java
+++ src/main/java/thePackmaster/cards/transmutationpack/CrystalResonance.java
@@ -1,8 +1,10 @@
 package thePackmaster.cards.transmutationpack;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.powers.transmutationpack.CrystalResonancePower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.*;
@@ -16,7 +18,7 @@ public class CrystalResonance extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        addToBot(new ApplyPowerAction(p, p, new CrystalResonancePower(p)));
     }
 
     public void upp() {
