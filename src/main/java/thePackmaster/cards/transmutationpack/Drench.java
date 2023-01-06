b5c62306840dfc82e225019083f29ba2a57eb9e2
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:09:36 -0800

    implement Drench

--- src/main/java/thePackmaster/cards/transmutationpack/Drench.java
+++ src/main/java/thePackmaster/cards/transmutationpack/Drench.java
@@ -1,13 +1,19 @@
 package thePackmaster.cards.transmutationpack;
 
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.cardmodifiers.transmutationpack.AbstractExtraEffectModifier;
+import thePackmaster.cardmodifiers.transmutationpack.DamageEffect;
+import thePackmaster.cardmodifiers.transmutationpack.GainBlockEffect;
 import thePackmaster.cards.AbstractPackmasterCard;
+
+import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.*;
 
-public class Drench extends AbstractHydrologistCard {
+public class Drench extends AbstractHydrologistCard implements TransmutableCard {
     public final static String ID = makeID("Drench");
     // intellij stuff ATTACK, ENEMY, UNCOMMON, 6, 2, 5, 2, , 
 
@@ -18,11 +24,20 @@ public class Drench extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        atb(new GainBlockAction(p, block));
+        hydrologistDamage(p, m, damage);
     }
 
     public void upp() {
         upgradeDamage(2);
         upgradeBlock(2);
+    }
+
+    @Override
+    public ArrayList<AbstractExtraEffectModifier> getMutableAbilities() {
+        ArrayList<AbstractExtraEffectModifier> list = new ArrayList<>();
+        list.add(new GainBlockEffect(this, true, 1));
+        list.add(new DamageEffect(this, true, 1));
+        return list;
     }
 }
