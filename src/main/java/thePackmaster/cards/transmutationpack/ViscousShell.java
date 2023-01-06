38f8801edb4f0e34bc8b6210f093948b6b8ae9ea
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:05:13 -0800

    implement Viscous Shell

--- src/main/java/thePackmaster/cards/transmutationpack/ViscousShell.java
+++ src/main/java/thePackmaster/cards/transmutationpack/ViscousShell.java
@@ -1,13 +1,18 @@
 package thePackmaster.cards.transmutationpack;
 
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.actions.transmutationpack.TransmuteCardAction;
+import thePackmaster.cardmodifiers.transmutationpack.AbstractExtraEffectModifier;
+import thePackmaster.cardmodifiers.transmutationpack.GainBlockEffect;
+
+import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.*;
 
-public class ViscousShell extends AbstractHydrologistCard {
+public class ViscousShell extends AbstractHydrologistCard implements TransmutableCard {
     public final static String ID = makeID("ViscousShell");
     // intellij stuff SKILL, SELF, COMMON, , , 5, 3, , 
 
@@ -17,10 +22,18 @@ public class ViscousShell extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        atb(new GainBlockAction(p, p, block));
+        atb(new TransmuteCardAction(this));
     }
 
     public void upp() {
         upgradeBlock(3);
+    }
+
+    @Override
+    public ArrayList<AbstractExtraEffectModifier> getMutableAbilities() {
+        ArrayList<AbstractExtraEffectModifier> list = new ArrayList<>();
+        list.add(new GainBlockEffect(this, true, 1));
+        return list;
     }
 }
