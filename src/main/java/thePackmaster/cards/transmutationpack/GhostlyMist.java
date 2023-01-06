9ae05e8620f7b9aff152a0db7f94bdb267108f20
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:13:57 -0800

    implement Ghostly Mist

--- src/main/java/thePackmaster/cards/transmutationpack/GhostlyMist.java
+++ src/main/java/thePackmaster/cards/transmutationpack/GhostlyMist.java
@@ -1,13 +1,19 @@
 package thePackmaster.cards.transmutationpack;
 
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.transmutationpack.TransmuteCardAction;
+import thePackmaster.cardmodifiers.transmutationpack.*;
 import thePackmaster.cards.AbstractPackmasterCard;
+
+import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.*;
 
-public class GhostlyMist extends AbstractHydrologistCard {
+public class GhostlyMist extends AbstractHydrologistCard implements TransmutableCard {
     public final static String ID = makeID("GhostlyMist");
     // intellij stuff skill, none, rare, , , , , 5, 
 
@@ -17,10 +23,22 @@ public class GhostlyMist extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        atb(new TransmuteCardAction(this));
     }
 
     public void upp() {
         upgradeBaseCost(0);
+    }
+    
+    @Override
+    public ArrayList<AbstractExtraEffectModifier> getMutableAbilities() {
+        ArrayList<AbstractExtraEffectModifier> list = new ArrayList<>();
+        list.add(new TransmuteSelfEffect(this, true));
+        return list;
+    }
+
+    @Override
+    public void onTransmuted(AbstractCard newCard) {
+        CardModifierManager.addModifier(newCard, new PurityModifier(magicNumber));
     }
 }
