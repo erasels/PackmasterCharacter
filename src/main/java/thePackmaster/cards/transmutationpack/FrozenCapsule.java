4effb0f44a6b20c382a57a5a744dcc441f76454d
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:11:46 -0800

    implement Frozen Capsule

--- src/main/java/thePackmaster/cards/transmutationpack/FrozenCapsule.java
+++ src/main/java/thePackmaster/cards/transmutationpack/FrozenCapsule.java
@@ -1,13 +1,20 @@
 package thePackmaster.cards.transmutationpack;
 
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.cardmodifiers.transmutationpack.AbstractExtraEffectModifier;
+import thePackmaster.cardmodifiers.transmutationpack.DamageEffect;
+import thePackmaster.cardmodifiers.transmutationpack.DrawCardEffect;
+import thePackmaster.cardmodifiers.transmutationpack.GainBlockEffect;
 import thePackmaster.cards.AbstractPackmasterCard;
+
+import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.*;
 
-public class FrozenCapsule extends AbstractHydrologistCard {
+public class FrozenCapsule extends AbstractHydrologistCard implements TransmutableCard {
     public final static String ID = makeID("FrozenCapsule");
     // intellij stuff attack, enemy, rare, 8, , , , 1, 1
 
@@ -18,10 +25,19 @@ public class FrozenCapsule extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        hydrologistDamage(p, m, damage);
+        atb(new DrawCardAction(p, magicNumber));
     }
 
     public void upp() {
         upgradeMagicNumber(1);
+    }
+
+    @Override
+    public ArrayList<AbstractExtraEffectModifier> getMutableAbilities() {
+        ArrayList<AbstractExtraEffectModifier> list = new ArrayList<>();
+        list.add(new DamageEffect(this, true, 1));
+        list.add(new DrawCardEffect(this, true, 1));
+        return list;
     }
 }
