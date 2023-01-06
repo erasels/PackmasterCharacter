dbbcbc0ef6846b9245abe1b41a1348120253447c
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:07:49 -0800

    implement Crystal Lattice

--- src/main/java/thePackmaster/cards/transmutationpack/CrystalLattice.java
+++ src/main/java/thePackmaster/cards/transmutationpack/CrystalLattice.java
@@ -1,7 +1,9 @@
 package thePackmaster.cards.transmutationpack;
 
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.transmutationpack.TransmuteCardAction;
 import thePackmaster.cards.AbstractPackmasterCard;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -14,10 +16,15 @@ public class CrystalLattice extends AbstractHydrologistCard {
     public CrystalLattice() {
         super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE, Subtype.ICE);
         baseMagicNumber = magicNumber = 2;
+        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        addToBot(new TransmuteCardAction((oldCard, newCard) -> {
+            for (int i = 0; i < magicNumber; ++i) {
+                addToTop(new MakeTempCardInHandAction(newCard));
+            }
+        }));
     }
 
     public void upp() {
