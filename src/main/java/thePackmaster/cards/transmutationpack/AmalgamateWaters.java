54711c3bd8b4410981388941d39d3e8e742ca1a0
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-06 10:20:49 -0800

    implement Amalgamate Waters

--- src/main/java/thePackmaster/cards/transmutationpack/AmalgamateWaters.java
+++ src/main/java/thePackmaster/cards/transmutationpack/AmalgamateWaters.java
@@ -1,7 +1,12 @@
 package thePackmaster.cards.transmutationpack;
 
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.transmutationpack.TransmuteCardAction;
+import thePackmaster.cardmodifiers.transmutationpack.AbstractExtraEffectModifier;
 import thePackmaster.cards.AbstractPackmasterCard;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -16,7 +21,22 @@ public class AmalgamateWaters extends AbstractHydrologistCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        AbstractCard amalgam = this;
+        atb(new TransmuteCardAction(true, (oldCard, newCard) -> {
+            for (AbstractCardModifier mod : CardModifierManager.modifiers(oldCard)) {
+                if (mod instanceof AbstractExtraEffectModifier) {
+                    AbstractExtraEffectModifier effect = (AbstractExtraEffectModifier)mod;
+                    if (effect.isMutable) {
+                        CardModifierManager.addModifier(amalgam, effect.makeCopy());
+                    }
+                }
+            }
+            if (oldCard instanceof TransmutableCard) {
+                ((TransmutableCard)oldCard).getMutableAbilities().forEach(mod -> {
+                    CardModifierManager.addModifier(amalgam, mod);
+                });
+            }
+        }, card -> true));
     }
 
     public void upp() {
