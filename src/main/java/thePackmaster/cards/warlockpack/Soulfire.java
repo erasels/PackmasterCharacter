d70bf45c3649e35874118911f982e1e9d8e19999
modargo <modargo@gmail.com>
2023-01-08 14:32:21 -0500

    Warlock pack: increase Soulfire damage by 2

--- src/main/java/thePackmaster/cards/warlockpack/Soulfire.java
+++ src/main/java/thePackmaster/cards/warlockpack/Soulfire.java
@@ -16,7 +16,7 @@ public class Soulfire extends AbstractPackmasterCard {
 
     public Soulfire() {
         super(ID, COST, AbstractCard.CardType.ATTACK, AbstractCard.CardRarity.COMMON, AbstractCard.CardTarget.ENEMY);
-        baseDamage = 8;
+        baseDamage = 10;
     }
 
     @Override
