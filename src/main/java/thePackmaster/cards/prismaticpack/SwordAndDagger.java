e7c95e950dc4bba7816cf287c9ed4abe8e32e451
modargo <modargo@gmail.com>
2023-01-07 19:02:51 -0500

    Prismatic pack: correct Sword and Dagger cost and reduce damage by 1

--- src/main/java/thePackmaster/cards/prismaticpack/SwordAndDagger.java
+++ src/main/java/thePackmaster/cards/prismaticpack/SwordAndDagger.java
@@ -17,8 +17,8 @@ public class SwordAndDagger extends AbstractPackmasterCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 0;
-    private static final int DAMAGE = 8;
+    private static final int COST = 1;
+    private static final int DAMAGE = 7;
     private static final int SHIVS = 1;
     private static final int UPGRADE_SHIVS = 1;
 
