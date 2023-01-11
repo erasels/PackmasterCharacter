3e207768c71611f97df37d9d59ffeeffc075fc0e
AlexMdle <92187869+AlexMdle@users.noreply.github.com>
2023-01-11 20:28:02 +0100

    - headshot fixes (#120)

--- src/main/java/thePackmaster/cards/hermitpack/Headshot.java
+++ src/main/java/thePackmaster/cards/hermitpack/Headshot.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.patches.hermitpack.EnumPatch;
+import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -15,6 +16,7 @@ public class Headshot extends AbstractHermitCard {
 
     private static final int DAMAGE = 7;
     private static final int UPGRADE_PLUS_DMG = 2;
+    public boolean trig_deadon = false;
 
     public Headshot() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
@@ -25,6 +27,13 @@ public class Headshot extends AbstractHermitCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(
                 new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), EnumPatch.HERMIT_GUN));
+    }
+
+    @Override
+    public void resetAttributes()
+    {
+        super.resetAttributes();
+        this.trig_deadon = false;
     }
 
     public void triggerOnGlowCheck() {
@@ -46,13 +55,38 @@ public class Headshot extends AbstractHermitCard {
         isDamageModified = damage != baseDamage;
     }
 
+    public void applyPowers() {
+        super.applyPowers();
+        if (isDeadOnPos()) {
+            this.damage *= 2;
+            this.isDamageModified = true;
+        }
+    }
+
     public boolean isDeadOnPos()
     {
+        if (this.trig_deadon)
+        return true;
+
+        if (!Wiz.p().hand.contains(this))
+        return false;
+
         double hand_pos = (AbstractDungeon.player.hand.group.indexOf(this)+0.5);
         double hand_size = (AbstractDungeon.player.hand.size());
         double relative = Math.abs(hand_pos-hand_size/2);
 
         return (relative<1);
+    }
+
+    @Override
+    public AbstractCard makeStatEquivalentCopy() {
+        AbstractCard cardcopy = super.makeStatEquivalentCopy();
+
+        if (AbstractDungeon.player != null)
+        if (this.isDeadOnPos())
+        ((Headshot)cardcopy).trig_deadon = true;
+
+        return cardcopy;
     }
 
     //Upgraded stats.
