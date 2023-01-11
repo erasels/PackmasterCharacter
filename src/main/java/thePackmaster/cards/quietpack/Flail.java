81926ad5f37bdef61944bdd6ba38e8441e03e13d
Paul-Sml <paul.semmel.pro@gmail.com>
2023-01-11 20:43:17 +0100

    mostly vfx quiet & warlock

--- src/main/java/thePackmaster/cards/quietpack/Flail.java
+++ src/main/java/thePackmaster/cards/quietpack/Flail.java
@@ -2,12 +2,15 @@ package thePackmaster.cards.quietpack;
 
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 import thePackmaster.cards.AbstractPackmasterCard;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
+import static thePackmaster.util.Wiz.atb;
 
 
 public class Flail extends AbstractPackmasterCard {
@@ -15,18 +18,20 @@ public class Flail extends AbstractPackmasterCard {
 
     public Flail() {
         super(ID, 2, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = 25;
+        baseDamage = 13;
         cardsToPreview = new Weight();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new VFXAction(new FlashAtkImgEffect(m.hb.cX-30.0f, m.hb.cY-30.0f, AbstractGameAction.AttackEffect.BLUNT_HEAVY)));
         dmg(m, AbstractGameAction.AttackEffect.NONE);
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
         this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeCopy(),2));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(6);
+        upgradeDamage(3);
     }
 }
 
