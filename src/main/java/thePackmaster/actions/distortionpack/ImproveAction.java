7df0d653eccb371cacb01e4ef6105e5b227623d3
Alchyr <isithalchyr@gmail.com>
2023-01-12 01:44:42 -0800

    distortion vfx optimization

--- src/main/java/thePackmaster/actions/distortionpack/ImproveAction.java
+++ src/main/java/thePackmaster/actions/distortionpack/ImproveAction.java
@@ -1,184 +1,44 @@
 package thePackmaster.actions.distortionpack;
 
-import basemod.ReflectionHacks;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import thePackmaster.patches.BetterPowerNegationCheckPatch;
-
-import java.util.ArrayList;
+import thePackmaster.vfx.distortionpack.ImproveEffect;
 
 public class ImproveAction extends AbstractGameAction {
-    private static final Logger logger = LogManager.getLogger("Distortion");
-    
-    private static final ArrayList<Texture> tempTextures = new ArrayList<>();
-
-    private ApplyPowerAction distortionApplier = null;
-
-
-    public static void _clean() {
-        for (Texture t : tempTextures) {
-            t.dispose();
-        }
-
-        tempTextures.clear();
-    }
-
-
     private AbstractMonster materia;
+    private ApplyPowerAction distortionApplier;
 
-    public ImproveAction(AbstractMonster m, ApplyPowerAction distortionApplier) {
-        this(m);
+    public ImproveAction(AbstractMonster m, int power, ApplyPowerAction distortionApplier) {
+        this.actionType = ActionType.SPECIAL;
+        this.materia = m;
+        this.amount = Math.min(15, power);
         this.distortionApplier = distortionApplier;
     }
+    public ImproveAction(AbstractMonster m, ApplyPowerAction distortionApplier) {
+        this(m, 10, distortionApplier);
+    }
 
-    public ImproveAction(AbstractMonster target) {
-        this.materia = target;
-        this.actionType = ActionType.SPECIAL;
+    public ImproveAction(AbstractMonster m) {
+        this(m, null);
     }
 
     public void update() {
-        if (distortionApplier != null && !BetterPowerNegationCheckPatch.Field.appliedSuccess.get(distortionApplier)) {
-            this.isDone = true;
+        this.isDone = true;
+        if (materia == null || (distortionApplier != null && !BetterPowerNegationCheckPatch.Field.appliedSuccess.get(distortionApplier))) {
             return;
         }
 
-        try {
-            TextureAtlas _form = ReflectionHacks.getPrivate(this.materia, AbstractCreature.class, "atlas");
-            if (_form != null) {
-                ArrayList<Texture> regionTextures = new ArrayList<>();
-                _form.getRegions();
-
-                for (TextureRegion r : _form.getRegions()) {
-                    regionTextures.add(r.getTexture());
-                }
-
-                for(int i = 0; i < regionTextures.size(); ++i) {
-                    _form.getTextures().remove(regionTextures.get(i));
-                    Texture _reconstructed = _refactor(regionTextures.get(i), true);
-                    regionTextures.set(i, _reconstructed);
-                    _form.getTextures().add(_reconstructed);
-                }
-
-                for (TextureRegion r : _form.getRegions()) {
-                    r.setTexture(regionTextures.remove(0));
-                }
-
-            } else {
-                Texture img = ReflectionHacks.getPrivate(this.materia, AbstractMonster.class, "img");
-                if (img != null) {
-                    img = _refactor(img, false);
-                    tempTextures.add(img);
-                    ReflectionHacks.setPrivate(this.materia, AbstractMonster.class, "img", img);
-                } else {
-                    logger.error("Materia has no data: " + this.materia.id);
-                }
+        for (AbstractGameEffect e : AbstractDungeon.effectList) {
+            if (e instanceof ImproveEffect) {
+                if (materia.equals(((ImproveEffect) e).m))
+                    return;
             }
-        } catch (Exception e) {
-            logger.error("Failed to reconstruct materia: " + this.materia.id);
-            e.printStackTrace();
         }
 
-        this.isDone = true;
-    }
-
-    public static Texture _refactor(Texture t) {
-        return _refactor(t, false);
-    }
-
-    public static Texture _refactor(Texture t, boolean dispose) {
-        try {
-            if (!t.getTextureData().isPrepared()) {
-                t.getTextureData().prepare();
-            }
-
-            Pixmap re = t.getTextureData().consumePixmap();
-            int altered;
-            int alterations;
-            int[][] area;
-            int startX;
-            int startY;
-            int initX;
-            int i;
-            int initY;
-            int b;
-            if (MathUtils.randomBoolean()) {
-                altered = 0;
-
-                for(alterations = MathUtils.random(11, 17); altered < alterations; ++altered) {
-                    area = new int[MathUtils.random(t.getWidth() / 8, t.getWidth() / 5)][MathUtils.random(t.getHeight() / 5, t.getHeight() / 3)];
-                    startX = MathUtils.random(0, re.getWidth() - area.length);
-                    startY = MathUtils.random(0, re.getHeight() - area[0].length);
-                    initX = startX;
-
-                    for(i = 0; i < area.length; i = ++initX - startX) {
-                        initY = startY;
-
-                        for(b = 0; b < area[0].length; b = ++initY - startY) {
-                            area[i][b] = re.getPixel(initX, initY);
-                        }
-                    }
-
-                    startY += MathUtils.random(t.getHeight() / 9, t.getHeight() / 5) * (MathUtils.randomBoolean() ? 1 : -1);
-                    initX = startX;
-
-                    for(i = 0; i < area.length; i = ++initX - startX) {
-                        initY = startY;
-
-                        for(b = 0; b < area[0].length; b = ++initY - startY) {
-                            if (initY >= 0 && initY <= re.getHeight()) {
-                                re.drawPixel(initX, initY, area[i][b]);
-                            }
-                        }
-                    }
-                }
-            } else {
-                altered = 0;
-
-                for(alterations = MathUtils.random(11, 17); altered < alterations; ++altered) {
-                    area = new int[MathUtils.random(t.getWidth() / 5, t.getWidth() / 3)][MathUtils.random(t.getHeight() / 8, t.getHeight() / 5)];
-                    startX = MathUtils.random(0, re.getWidth() - area.length);
-                    startY = MathUtils.random(0, re.getHeight() - area[0].length);
-                    initX = startX;
-
-                    for(i = 0; i < area.length; i = ++initX - startX) {
-                        initY = startY;
-
-                        for(b = 0; b < area[0].length; b = ++initY - startY) {
-                            area[i][b] = re.getPixel(initX, initY);
-                        }
-                    }
-
-                    startX += MathUtils.random(t.getWidth() / 9, t.getWidth() / 5) * (MathUtils.randomBoolean() ? 1 : -1);
-                    initX = startX;
-
-                    for(i = 0; i < area.length; i = ++initX - startX) {
-                        initY = startY;
-
-                        for(b = 0; b < area[0].length; b = ++initY - startY) {
-                            if (initX >= 0 && initX <= re.getWidth()) {
-                                re.drawPixel(initX, initY, area[i][b]);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (dispose) {
-                t.dispose();
-            }
-
-            return new Texture(re);
-        } catch (Exception var12) {
-            return t;
-        }
+        AbstractDungeon.effectList.add(new ImproveEffect(materia, amount));
     }
 }
---
+++ src/main/java/thePackmaster/vfx/distortionpack/ImproveEffect.java
@@ -0,0 +1,203 @@
+package thePackmaster.vfx.distortionpack;
+
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ImproveEffect extends AbstractGameEffect {
+    private static final Logger logger = LogManager.getLogger("Distortion");
+
+    private static final ArrayList<Texture> tempTextures = new ArrayList<>();
+
+    public static void _clean() {
+        for (Texture t : tempTextures) {
+            t.dispose();
+        }
+
+        tempTextures.clear();
+    }
+
+
+    public final AbstractMonster m;
+    private final List<NotFinalPair<Texture, Consumer<Texture>>> textureSetters = new ArrayList<>();
+    private final int amount;
+
+    public ImproveEffect(AbstractMonster m, int amount) {
+        this.m = m;
+        this.amount = amount;
+
+        try {
+            TextureAtlas _form = ReflectionHacks.getPrivate(this.m, AbstractCreature.class, "atlas");
+            if (_form != null) {
+                for (TextureRegion r : _form.getRegions()) {
+                    textureSetters.add(new NotFinalPair<>(r.getTexture(), r::setTexture));
+                }
+            } else {
+                Texture img = ReflectionHacks.getPrivate(this.m, AbstractMonster.class, "img");
+                if (img != null) {
+                    textureSetters.add(new NotFinalPair<>(img, (t)->ReflectionHacks.setPrivate(m, AbstractMonster.class, "img", t)));
+                } else {
+                    logger.error("Materia has no data: " + this.m.id);
+                    this.isDone = true;
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Failed to reconstruct m: " + this.m.id);
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void update() {
+        if (textureSetters.isEmpty()) {
+            isDone = true;
+            return;
+        }
+
+        NotFinalPair<Texture, Consumer<Texture>> tex = textureSetters.remove(textureSetters.size() - 1);
+        tex.b.accept(_refactor(tex.a, amount));
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    private static final float MAX_AREA = 10000;
+
+    public static Texture _refactor(Texture t) {
+        return _refactor(t, false);
+    }
+
+    public static Texture _refactor(Texture t, int alteration) {
+        return _refactor(t, alteration, false);
+    }
+
+    public static Texture _refactor(Texture t, boolean dispose) {
+        return _refactor(t, 10, dispose);
+    }
+
+    public static Texture _refactor(Texture t, int alteration, boolean dispose) {
+        try {
+            if (!t.getTextureData().isPrepared()) {
+                t.getTextureData().prepare();
+            }
+
+            Pixmap re = t.getTextureData().consumePixmap();
+            int altered;
+            int alterations;
+            int[][] area;
+            int startX;
+            int startY;
+            int initX;
+            int i;
+            int initY;
+            int b;
+            if (MathUtils.randomBoolean()) {
+                altered = 0;
+
+                for (alterations = alteration + MathUtils.random(-2, 2); altered < alterations; ++altered) {
+                    int width = MathUtils.random(t.getWidth() / 8, t.getWidth() / 5);
+                    int height = MathUtils.random(t.getHeight() / 5, t.getHeight() / 3);
+                    float scale = (width * height) / MAX_AREA;
+                    if (scale > 1) {
+                        width /= Math.sqrt(scale);
+                        height /= Math.sqrt(scale);
+                    }
+                    area = new int[width][height];
+                    startX = MathUtils.random(0, re.getWidth() - area.length);
+                    startY = MathUtils.random(0, re.getHeight() - area[0].length);
+                    initX = startX;
+
+                    for(i = 0; i < area.length; i = ++initX - startX) {
+                        initY = startY;
+
+                        for(b = 0; b < area[0].length; b = ++initY - startY) {
+                            area[i][b] = re.getPixel(initX, initY);
+                        }
+                    }
+
+                    startY += MathUtils.random(t.getHeight() / 9, t.getHeight() / 5) * (MathUtils.randomBoolean() ? 1 : -1);
+                    initX = startX;
+
+                    for(i = 0; i < area.length; i = ++initX - startX) {
+                        initY = startY;
+
+                        for(b = 0; b < area[0].length; b = ++initY - startY) {
+                            if (initY >= 0 && initY <= re.getHeight()) {
+                                re.drawPixel(initX, initY, area[i][b]);
+                            }
+                        }
+                    }
+                }
+            } else {
+                altered = 0;
+
+                for(alterations = MathUtils.random(11, 17); altered < alterations; ++altered) {
+                    area = new int[MathUtils.random(t.getWidth() / 5, t.getWidth() / 3)][MathUtils.random(t.getHeight() / 8, t.getHeight() / 5)];
+                    startX = MathUtils.random(0, re.getWidth() - area.length);
+                    startY = MathUtils.random(0, re.getHeight() - area[0].length);
+                    initX = startX;
+
+                    for(i = 0; i < area.length; i = ++initX - startX) {
+                        initY = startY;
+
+                        for(b = 0; b < area[0].length; b = ++initY - startY) {
+                            area[i][b] = re.getPixel(initX, initY);
+                        }
+                    }
+
+                    startX += MathUtils.random(t.getWidth() / 9, t.getWidth() / 5) * (MathUtils.randomBoolean() ? 1 : -1);
+                    initX = startX;
+
+                    for(i = 0; i < area.length; i = ++initX - startX) {
+                        initY = startY;
+
+                        for(b = 0; b < area[0].length; b = ++initY - startY) {
+                            if (initX >= 0 && initX <= re.getWidth()) {
+                                re.drawPixel(initX, initY, area[i][b]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (dispose) {
+                t.dispose();
+            }
+
+            return new Texture(re);
+        } catch (Exception e) {
+            return t;
+        }
+    }
+
+    private static class NotFinalPair<A, B> {
+        A a;
+        B b;
+
+        public NotFinalPair(A a, B b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
