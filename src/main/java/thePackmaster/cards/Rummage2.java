c28ae4b52b82e97fb3b10ff2b8dc15708fd89120
Laugic <ajheb97@gmail.com>
2023-01-13 18:13:28 -0500

    Removed unused imports and strings

--- src/main/java/thePackmaster/cards/Rummage2.java
+++ src/main/java/thePackmaster/cards/Rummage2.java
@@ -1,16 +1,12 @@
 package thePackmaster.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.powers.ReboundTurnlessPower;
 import thePackmaster.util.Wiz;
 
-import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
