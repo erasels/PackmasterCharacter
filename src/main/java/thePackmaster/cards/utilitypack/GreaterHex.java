43792387b80c1e18cec8a712d36de9fa7cf56a4d
modargo <modargo@gmail.com>
2023-01-14 08:15:30 -0500

    Various packs: removed unneeded card strings lookups

--- src/main/java/thePackmaster/cards/utilitypack/GreaterHex.java
+++ src/main/java/thePackmaster/cards/utilitypack/GreaterHex.java
@@ -15,9 +15,6 @@ import thePackmaster.vfx.utilitypack.HexEffect;
 
 public class GreaterHex extends AbstractPackmasterCard {
     public static final String ID = SpireAnniversary5Mod.makeID("GreaterHex");
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String NAME = cardStrings.NAME;
-    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
     private static final int UPGRADE_COST = 0;
     private static final int STATS = 2;
--- src/main/java/thePackmaster/cards/utilitypack/RareStrike.java
+++ src/main/java/thePackmaster/cards/utilitypack/RareStrike.java
@@ -17,9 +17,6 @@ import java.util.List;
 
 public class RareStrike extends AbstractPackmasterCard {
     public static final String ID = SpireAnniversary5Mod.makeID("RareStrike");
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String NAME = cardStrings.NAME;
-    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
     private static final int DAMAGE = 6;
     private static final int EXTRA_DAMAGE = 2;
