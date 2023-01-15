07d4fc3480ad599bf5d86b32bf72cf3d5537bd33
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-15 00:33:59 -0800

    implement aqueducts

--- src/main/java/thePackmaster/cards/gowiththeflowpack/Aqueducts.java
+++ src/main/java/thePackmaster/cards/gowiththeflowpack/Aqueducts.java
@@ -1,8 +1,10 @@
 package thePackmaster.cards.gowiththeflowpack;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.cards.transmutationpack.AbstractHydrologistCard;
+import thePackmaster.powers.gowiththeflowpack.AqueductsPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -11,11 +13,14 @@ public class Aqueducts extends AbstractHydrologistCard {
 
     public Aqueducts() {
         super(ID, 2, CardType.POWER, CardRarity.UNCOMMON, CardTarget.NONE, Subtype.WATER);
+        magicNumber = baseMagicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new AqueductsPower(p, magicNumber), magicNumber));
     }
 
     public void upp() {
+        upgradeMagicNumber(1);
     }
 }
