6a4ef8e28b8518fb13ea4c35551f57d3d677ecf0
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-15 01:25:00 -0800

    implement storm front

--- src/main/java/thePackmaster/cards/gowiththeflowpack/StormFront.java
+++ src/main/java/thePackmaster/cards/gowiththeflowpack/StormFront.java
@@ -10,12 +10,22 @@ public class StormFront extends AbstractHydrologistCard {
     public final static String ID = makeID("StormFront");
 
     public StormFront() {
-        super(ID, 3, CardType.SKILL, CardRarity.RARE, CardTarget.NONE, Subtype.STEAM);
+        super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.NONE, Subtype.STEAM);
+        damage = baseDamage = 30;
+        magicNumber = baseMagicNumber = 10;
+        retain = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        hydrologistDamage(p, m, 30);
+    }
+
+    @Override
+    public void triggerOnManualDiscard() {
+        baseDamage += magicNumber;
     }
 
     public void upp() {
+        upgradeMagicNumber(5);
     }
 }
