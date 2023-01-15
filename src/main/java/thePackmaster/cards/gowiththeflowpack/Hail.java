67acfd0a8549e5f282c1b175d057bb08b648abdb
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-15 00:48:01 -0800

    implement hail

--- src/main/java/thePackmaster/cards/gowiththeflowpack/Hail.java
+++ src/main/java/thePackmaster/cards/gowiththeflowpack/Hail.java
@@ -1,8 +1,13 @@
 package thePackmaster.cards.gowiththeflowpack;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.orbs.Frost;
 import thePackmaster.cards.transmutationpack.AbstractHydrologistCard;
+
+import java.util.Iterator;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -11,11 +16,46 @@ public class Hail extends AbstractHydrologistCard {
 
     public Hail() {
         super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY, Subtype.ICE);
+        magicNumber = baseMagicNumber = 1;
+        damage = baseDamage = 9;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        int hits = magicNumber;
+        if (hits < 1) hits = 1;
+        for (int i = 0; i < hits; ++i) {
+            hydrologistDamage(p, m, damage);
+        }
+    }
+
+    public void applyPowers() {
+        super.applyPowers();
+        if (magicNumber > 1) {
+            rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+            initializeDescription();
+        }
+    }
+
+    public void onMoveToDiscard() {
+        this.rawDescription = cardStrings.DESCRIPTION;
+        this.initializeDescription();
+    }
+
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        if (magicNumber > 1) {
+            rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void triggerOnManualDiscard() {
+        baseMagicNumber++;
+        magicNumber++;
     }
 
     public void upp() {
+        upgradeDamage(3);
     }
 }
