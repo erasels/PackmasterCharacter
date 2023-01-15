7b27a9d6e303a16b0b4bdc3a539ef98c415db33c
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-15 01:16:31 -0800

    implement Pneumatics

--- src/main/java/thePackmaster/cards/gowiththeflowpack/Pneumatics.java
+++ src/main/java/thePackmaster/cards/gowiththeflowpack/Pneumatics.java
@@ -1,8 +1,11 @@
 package thePackmaster.cards.gowiththeflowpack;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.gowiththeflowpack.FlowAction;
 import thePackmaster.cards.transmutationpack.AbstractHydrologistCard;
+import thePackmaster.powers.gowiththeflowpack.PneumaticsPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -11,11 +14,15 @@ public class Pneumatics extends AbstractHydrologistCard {
 
     public Pneumatics() {
         super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE, Subtype.STEAM);
+        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new FlowAction());
+        addToBot(new ApplyPowerAction(p, p, new PneumaticsPower(p, 1), 1));
     }
 
     public void upp() {
+        exhaust = false;
     }
 }
