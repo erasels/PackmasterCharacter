7b48aa98ffd42ccb0b73da0fd72b89d9a1afc009
Alchyr <isithalchyr@gmail.com>
2023-01-15 12:51:47 -0800

    orbpack

---
+++ src/main/java/thePackmaster/cards/orbpack/Thunderstruck.java
@@ -0,0 +1,27 @@
+package thePackmaster.cards.orbpack;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.powers.orbpack.ThunderstruckPower;
+
+import static thePackmaster.SpireAnniversary5Mod.makeID;
+import static thePackmaster.util.Wiz.*;
+
+public class Thunderstruck extends AbstractPackmasterCard {
+    public final static String ID = makeID("Thunderstruck");
+    // intellij stuff power, none, rare, , , , , 1, 
+
+    public Thunderstruck() {
+        super(ID, 2, CardType.POWER, CardRarity.RARE, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new ThunderstruckPower(p, this.magicNumber));
+    }
+
+    public void upp() {
+        upgradeBaseCost(1);
+    }
+}
