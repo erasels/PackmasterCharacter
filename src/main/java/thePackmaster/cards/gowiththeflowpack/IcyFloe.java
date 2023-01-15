ce64aace6f3c9e9d21899260ac772f29c4f5019d
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-01-15 01:10:35 -0800

    implement Icy Floe

--- src/main/java/thePackmaster/cards/gowiththeflowpack/IcyFloe.java
+++ src/main/java/thePackmaster/cards/gowiththeflowpack/IcyFloe.java
@@ -1,8 +1,11 @@
 package thePackmaster.cards.gowiththeflowpack;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.gowiththeflowpack.FlowAction;
 import thePackmaster.cards.transmutationpack.AbstractHydrologistCard;
+import thePackmaster.powers.gowiththeflowpack.AccumulatorPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -11,11 +14,15 @@ public class IcyFloe extends AbstractHydrologistCard {
 
     public IcyFloe() {
         super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE, Subtype.ICE);
+        magicNumber = baseMagicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new AccumulatorPower(p, magicNumber), magicNumber));
+        addToBot(new FlowAction());
     }
 
     public void upp() {
+        upgradeMagicNumber(1);
     }
 }
