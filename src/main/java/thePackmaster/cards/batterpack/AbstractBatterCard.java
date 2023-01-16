f3bb26a24cbd61691b7927c6f821ea9d0ba5eb4d
mikemayhemdev <mikemayhemdev@gmail.com>
2023-01-16 14:16:58 -0500

    Unhook batter frame for now, till I get Alex's seal of approval.

--- src/main/java/thePackmaster/cards/batterpack/AbstractBatterCard.java
+++ src/main/java/thePackmaster/cards/batterpack/AbstractBatterCard.java
@@ -4,6 +4,6 @@ import thePackmaster.cards.AbstractPackmasterCard;
 
 public abstract class AbstractBatterCard extends AbstractPackmasterCard {
     public AbstractBatterCard(String cardID, int cost, CardType type, CardRarity rarity, CardTarget target) {
-        super(cardID, cost, type, rarity, target, "batter");
+        super(cardID, cost, type, rarity, target);
     }
 }
