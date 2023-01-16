bb5bca08df1d448bdd6398c68dd72f57d7565283
erasels <emanuelbedburdick@yahoo.de>
2023-01-16 16:01:53 +0100

    Make Tailflail stronger and AoE

--- src/main/java/thePackmaster/cards/sneckopack/TailFlail.java
+++ src/main/java/thePackmaster/cards/sneckopack/TailFlail.java
@@ -1,7 +1,6 @@
 package thePackmaster.cards.sneckopack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -17,16 +16,19 @@ public class TailFlail extends AbstractPackmasterCard {
 
     public TailFlail() {
         super(ID, 2, AbstractCard.CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
-        baseDamage = 16;
-        baseMagicNumber = magicNumber = 1;
+        baseDamage = 6;
+        baseSecondDamage = 16;
+        baseMagicNumber = magicNumber = 2;
+        isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        allDmg(AbstractGameAction.AttackEffect.BLUNT_HEAVY);
         addToBot(new RandomizeCostAction(magicNumber));
     }
 
     public void upp() {
-        upgradeMagicNumber(1);
+        upgradeDamage(6);
+        upgradeSecondDamage(2);
     }
 }
