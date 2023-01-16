a57c6b9b0d4e5db53a365530e93bf3e9ecc80774
mikemayhemdev <mikemayhemdev@gmail.com>
2023-01-16 04:32:18 -0500

    Cthulhu Pack iteration
    End of Days reworked, now generates dark based on unspent energy.
    
    Gaze the Void +1 base damage.  Nightmare Stance reworked - when exited, draw 2 cards, gain E, and lose 1 HP.  Exits automatically when you use Madness.
    
    Familial Curse changed - Instead of discarding the hand, grants Madness and 1 HP loss at the start of next turn.
    
    Star Spawn - Increase max orb traded for a Heal effect.  Costs 3 (up from 2).

--- src/main/java/thePackmaster/cards/cthulhupack/StarSpawn.java
+++ src/main/java/thePackmaster/cards/cthulhupack/StarSpawn.java
@@ -1,14 +1,11 @@
 package thePackmaster.cards.cthulhupack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
-import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
-import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.Plasma;
-import thePackmaster.cards.AbstractPackmasterCard;
-import thePackmaster.stances.cthulhupack.NightmareStance;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -16,24 +13,27 @@ public class StarSpawn extends AbstractCthulhuCard {
     public final static String ID = makeID("StarSpawn");
 
     private static final int ATTACK_DMG = 13;
-    private static final int HITS = 2;
-    private static final int UPGRADE_PLUS_ATTACK_DMG = 5;
+    private static final int HEAL = 6;
+    private static final int UPGRADE_PLUS_ATTACK_DMG = 3;
+    private static final int UPGRADE_HEAL = 3;
 
     public StarSpawn() {
-        super(ID, 2, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
+        super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
         this.damage = this.baseDamage = ATTACK_DMG;
-        this.magicNumber = this.baseMagicNumber = HITS;
+        this.magicNumber = this.baseMagicNumber = HEAL;
         exhaust = true;
+        tags.add(CardTags.HEALING);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.LIGHTNING);
         dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        addToBot(new IncreaseMaxOrbAction(1));
+        addToBot(new HealAction(p, p, magicNumber));
         addToBot(new ChannelAction(new Plasma()));
     }
 
     public void upp() {
         upgradeDamage(UPGRADE_PLUS_ATTACK_DMG);
+        upgradeMagicNumber(UPGRADE_HEAL);
     }
 }
