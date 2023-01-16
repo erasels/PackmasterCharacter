4dd29be1dc77fdd2b71c8a9beede41f0cf8687db
NoMasPollo <65381377+NoMasPollo@users.noreply.github.com>
2023-01-15 17:45:07 -0800

    Add files via upload

---
+++ src/main/java/thePackmaster/powers/farmerpack/SupplyChainPower.java
@@ -0,0 +1,38 @@
+package thePackmaster.powers.farmerpack;
+
+
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import thePackmaster.cards.farmerpack.Fertilizer;
+import thePackmaster.powers.AbstractPackmasterPower;
+import thePackmaster.powers.entropypack.EntropyPower;
+
+import static thePackmaster.SpireAnniversary5Mod.makeID;
+
+public class SupplyChainPower extends AbstractPackmasterPower {
+    public static final String POWER_ID = makeID("SupplyChainPower");
+    private static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
+    private static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
+
+    public SupplyChainPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+        updateDescription();
+    }
+    @Override
+    public void atStartOfTurn() {
+        this.flashWithoutSound();
+        for (int i = 0; i < amount; i++) {
+            addToBot(new MakeTempCardInHandAction(new Fertilizer()));
+        }
+
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+}
