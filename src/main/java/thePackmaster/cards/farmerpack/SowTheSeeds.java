42c284fdd2ff86fa19d303f120978fff0cf4730d
NoMasPollo <65381377+NoMasPollo@users.noreply.github.com>
2023-01-17 08:14:01 -0800

    Update SowTheSeeds.java

--- src/main/java/thePackmaster/cards/farmerpack/SowTheSeeds.java
+++ src/main/java/thePackmaster/cards/farmerpack/SowTheSeeds.java
@@ -1,12 +1,19 @@
 package thePackmaster.cards.farmerpack;
 
+import basemod.BaseMod;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.actions.pixiepack.DrawSpecificCardAction;
+import thePackmaster.util.Wiz;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -29,34 +36,47 @@ public class SowTheSeeds extends AbstractFarmerCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         atb(new MakeTempCardInHandAction(new Fertilizer()));
-        Iterator<AbstractCard> typeDetect = AbstractDungeon.player.drawPile.group.iterator();
-        while (typeDetect.hasNext()) {
-            AbstractCard i = typeDetect.next();
-            if (i.type == CardType.ATTACK && !attack) {
-                count += 1;
-                att(new DrawSpecificCardAction(i));
-                attack = true;
-            } else if (i.type == CardType.SKILL && !skill) {
-                count += 1;
-                att(new DrawSpecificCardAction(i));
-                skill = true;
-            } else if (i.type == CardType.POWER && !power) {
-                count += 1;
-                att(new DrawSpecificCardAction(i));
-                power = true;
-            } else if (i.type == CardType.STATUS && !status) {
-                count += 1;
-                att(new DrawSpecificCardAction(i));
-                status = true;
-            } else if (i.type == CardType.CURSE && !curse) {
-                count += 1;
-                att(new DrawSpecificCardAction(i));
-                curse = true;
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+
+
+                ArrayList<AbstractCard> targets = new ArrayList<>();
+                for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
+                    if (SpireAnniversary5Mod.cardParentMap.get(c.cardID) != null) {
+                        targets.add(c);
+                    }
+                }
+
+                //Add cards equals to magicNumber into your hand, if into discard
+                for (int i = 0; i < magicNumber; i++) {
+                    if (targets.isEmpty()) return;
+
+                    AbstractCard current = Wiz.getRandomItem(targets, AbstractDungeon.cardRandomRng);
+                    if (current != null) {
+                        if (AbstractDungeon.player.hand.size() == BaseMod.MAX_HAND_SIZE) {
+                            AbstractDungeon.player.drawPile.moveToDiscardPile(current);
+                            AbstractDungeon.player.createHandIsFullDialog();
+                        } else {
+                            current.unhover();
+                            current.lighten(true);
+                            current.setAngle(0.0F);
+                            current.drawScale = 0.12F;
+                            current.targetDrawScale = 0.75F;
+                            current.current_x = CardGroup.DRAW_PILE_X;
+                            current.current_y = CardGroup.DRAW_PILE_Y;
+                            AbstractDungeon.player.drawPile.removeCard(current);
+                            AbstractDungeon.player.hand.addToTop(current);
+                            AbstractDungeon.player.hand.refreshHandLayout();
+                            AbstractDungeon.player.hand.applyPowers();
+                        }
+                        targets.removeIf(c -> c.type == current.type);
+                        //targets.removeIf(c -> SpireAnniversary5Mod.cardParentMap.get(c.CardType).equals(current.Card));
+                    }
+                }
             }
-            if (count >= magicNumber) {
-                break;
-            }
-        }
+        });
     }
 
     public void upp() {
