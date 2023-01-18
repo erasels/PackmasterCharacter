2f0a8122d3a79d80a541066f7d732734ed990b7d
Vex Darkon <edanvirgo@gmail.com>
2023-01-17 18:16:33 -0700

    Darkside Slap and Retromation Power fixes

--- src/main/java/thePackmaster/cards/contentcreatorpack/DarksideSlap.java
+++ src/main/java/thePackmaster/cards/contentcreatorpack/DarksideSlap.java
@@ -3,18 +3,16 @@ package thePackmaster.cards.contentcreatorpack;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.cards.AbstractPackmasterCard;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
-public class DarksideSlap extends AbstractContentCard {
+public class DarksideSlap extends AbstractContentCard implements MegaEtherealCard {
     public final static String ID = makeID("DarksideSlap");
+
     public DarksideSlap() {
         super(ID, 1, CardType.ATTACK, CardRarity.SPECIAL, CardTarget.ENEMY);
         baseDamage = 25;
     }
-
-    //TODO - Megathereal effect
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.FIRE);
