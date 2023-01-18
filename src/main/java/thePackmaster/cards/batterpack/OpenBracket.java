1b6115fdc282a6b7101c2ef2588076592a2aece2
AlexMdle <92187869+AlexMdle@users.noreply.github.com>
2023-01-18 08:41:59 +0100

    - open bracket rework

--- src/main/java/thePackmaster/cards/batterpack/OpenBracket.java
+++ src/main/java/thePackmaster/cards/batterpack/OpenBracket.java
@@ -1,17 +1,10 @@
 package thePackmaster.cards.batterpack;
 
-import basemod.BaseMod;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.NoDrawPower;
-import thePackmaster.cards.AbstractPackmasterCard;
-import thePackmaster.util.Wiz;
+import thePackmaster.powers.batterpack.OpenBracketPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -25,53 +18,8 @@ public class OpenBracket extends AbstractBatterCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new AbstractGameAction() {
-            @Override
-            public void update() {
-                if (p.drawPile.isEmpty()) {
-                    this.isDone = true;
-                    return;
-                }
-
-                // Add attacks to group.
-                CardGroup tmp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
-
-                for(AbstractCard card : p.drawPile.group)
-                {
-                    if (card.type == CardType.ATTACK)
-                    tmp.addToRandomSpot(card);
-                }
-
-                if (tmp.size() == 0) {
-                    this.isDone = true;
-                    return;
-                }
-
-                AbstractCard caard;
-
-                // Put attacks from group on top and draw.
-                for(int i = 0; i < OpenBracket.this.magicNumber; ++i) {
-                    if (!tmp.isEmpty()) {
-                        tmp.shuffle();
-                        caard = tmp.getBottomCard();
-                        tmp.removeCard(caard);
-                        if (p.hand.size() == BaseMod.MAX_HAND_SIZE) {
-                            p.createHandIsFullDialog();
-                        } else {
-                            if (!AbstractDungeon.player.hasPower(NoDrawPower.POWER_ID))
-                            {
-                                p.drawPile.group.remove(caard);
-                                p.drawPile.addToTop(caard);
-                                this.addToTop(new DrawCardAction(1));
-                            }
-                        }
-                    }
-                }
-
-                this.isDone = true;
-            }
-        });
-        this.addToBot(new ApplyPowerAction(p, p, new NoDrawPower(p)));
+        this.addToBot(new DrawCardAction(this.magicNumber));
+        this.addToBot(new ApplyPowerAction(p, p, new OpenBracketPower(p,-1),-1));
     }
 
     public void upp() {
