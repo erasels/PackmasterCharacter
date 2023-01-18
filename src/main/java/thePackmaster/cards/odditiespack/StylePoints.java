d6b499980e42a74e83e7a57a6bc8dbdd839d87d5
Vex Darkon <edanvirgo@gmail.com>
2023-01-17 21:44:22 -0700

    Style points healing filter

--- src/main/java/thePackmaster/cards/odditiespack/StylePoints.java
+++ src/main/java/thePackmaster/cards/odditiespack/StylePoints.java
@@ -8,11 +8,12 @@ import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
 import thePackmaster.SpireAnniversary5Mod;
-import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.hats.Hats;
 import thePackmaster.packs.AbstractCardPack;
 import thePackmaster.packs.CoreSetPack;
 import thePackmaster.util.Wiz;
+
+import java.util.ArrayList;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.atb;
@@ -34,7 +35,14 @@ public class StylePoints extends AbstractOdditiesCard {
         if (Hats.currentHat != null) {
             pack = SpireAnniversary5Mod.packsByID.get(Hats.currentHat);
         }
-        AbstractCard tar = CardLibrary.getCard(Wiz.getRandomItem(pack.getCards(), AbstractDungeon.cardRandomRng));
+        ArrayList<AbstractCard> possCards = new ArrayList<>();
+        for (String cardID : pack.getCards()) {
+            AbstractCard test = CardLibrary.getCard(cardID).makeCopy();
+            if (test.rarity != CardRarity.SPECIAL && !test.hasTag(CardTags.HEALING)) {
+                possCards.add(test);
+            }
+        }
+        AbstractCard tar = Wiz.getRandomItem(possCards, AbstractDungeon.cardRandomRng);
         makeInHand(tar);
     }
 
