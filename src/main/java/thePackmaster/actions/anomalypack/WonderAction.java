11254fcdad0d3afe539c744c04ca72ce1d45e31f
erasels <emanuelbedburdick@yahoo.de>
2023-01-22 13:11:50 +0100

    Fix Wonder action setting energy too low

--- src/main/java/thePackmaster/actions/anomalypack/WonderAction.java
+++ src/main/java/thePackmaster/actions/anomalypack/WonderAction.java
@@ -1,14 +1,12 @@
 package thePackmaster.actions.anomalypack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.unique.LoseEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import thePackmaster.util.Wiz;
-
-import java.util.Iterator;
-
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 
 public class WonderAction extends AbstractGameAction {
     public WonderAction() {
@@ -19,7 +17,7 @@ public class WonderAction extends AbstractGameAction {
         this.tickDuration();
         if (this.isDone) {
             for (AbstractCard c : DrawCardAction.drawnCards) {
-                Wiz.atb(new LoseEnergyAction(Wiz.adp().energy.energy));
+                Wiz.atb(new LoseEnergyAction(EnergyPanel.totalCount));
                 Wiz.atb(new GainEnergyAction(Wiz.getLogicalCardCost(c)));
             }
         }
