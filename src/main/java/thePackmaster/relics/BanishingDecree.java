47482b24eb7f76599361d32510055b74527ac364
modargo <modargo@gmail.com>
2023-01-22 12:29:24 -0500

    Oddities pack: have Sleeve Up affect rewards generated by Banishing Decree or PM Booster Box

--- src/main/java/thePackmaster/relics/BanishingDecree.java
+++ src/main/java/thePackmaster/relics/BanishingDecree.java
@@ -2,6 +2,7 @@ package thePackmaster.relics;
 
 import basemod.BaseMod;
 import basemod.abstracts.CustomSavable;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.SpawnModificationCard;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -20,6 +21,7 @@ import thePackmaster.util.Wiz;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static thePackmaster.SpireAnniversary5Mod.*;
 
@@ -125,6 +127,10 @@ public class BanishingDecree extends AbstractPackmasterRelic implements CustomSa
                         for (AbstractCard c : reward.cards) {
                             relic.onPreviewObtainCard(c);
                         }
+                    }
+                    List<SpawnModificationCard> spawnModificationCards = reward.cards.stream().filter(c -> c instanceof SpawnModificationCard).map(c -> (SpawnModificationCard)c).collect(Collectors.toList());
+                    for (SpawnModificationCard c : spawnModificationCards) {
+                        c.onRewardListCreated(reward.cards);
                     }
                     AbstractDungeon.getCurrRoom().rewards.add(reward);
                 }
