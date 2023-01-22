88264e98a2c66366ec19fa90ecbe21617591a564
erasels <emanuelbedburdick@yahoo.de>
2023-01-22 22:46:53 +0100

    Add upgrade for searing Light

--- src/main/java/thePackmaster/cards/dragonwrathpack/SearingLight.java
+++ src/main/java/thePackmaster/cards/dragonwrathpack/SearingLight.java
@@ -1,23 +1,10 @@
 package thePackmaster.cards.dragonwrathpack;
 
-import com.evacipated.cardcrawl.mod.stslib.actions.common.DamageCallbackAction;
-import com.evacipated.cardcrawl.mod.stslib.actions.common.GainCustomBlockAction;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-
 import com.megacrit.cardcrawl.cards.red.FlameBarrier;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.orbs.Lightning;
-import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
-import thePackmaster.actions.dragonwrathpack.SmiteAction;
 import thePackmaster.orbs.dragonwrathpack.LightOrb;
 
 import static thePackmaster.SpireAnniversary5Mod.makeCardPath;
@@ -27,24 +14,11 @@ public class SearingLight extends AbstractDragonwrathCard {
 
     public static final String ID = makeID(SearingLight.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
     public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("DivineJolt.png");
-    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
-
-
-    // /TEXT DECLARATION/
-
-
-    // STAT DECLARATIO
-
-    private static final int DAMAGE = 20;
-    private static final int UPGRADE_PLUS_DMG = 2;  // UPGRADE_PLUS_DMG = 2
-
-    // /STAT DECLARATION/
 
 
     public SearingLight(){
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         baseBlock = 8;
-        this.magicNumber = baseMagicNumber = 3;
     }
 
 
@@ -52,7 +26,7 @@ public class SearingLight extends AbstractDragonwrathCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         CardCrawlGame.sound.play("POWER_MANTRA", 0.05F);
-        addToBot(new GainBlockAction(AbstractDungeon.player,block));
+        blck();
         addToBot(new ChannelAction(new LightOrb()));
     }
 
@@ -64,8 +38,7 @@ public class SearingLight extends AbstractDragonwrathCard {
     // Upgraded stats.
     @Override
     public void upp() {
-        upgradeMagicNumber(-2);
-        upgradeDamage(UPGRADE_PLUS_DMG);
+        upgradeBlock(3);
     }
 }
 
