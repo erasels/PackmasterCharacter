d7b9351bc41b7cfa54574bfb7fde5da7faf4a758
Ocean <s@ocean.lol>
2023-01-23 17:28:17 +0000

    show pack name for vanilla cards in scv

--- src/main/java/thePackmaster/patches/SingleCardViewBorderTextPatch.java
+++ src/main/java/thePackmaster/patches/SingleCardViewBorderTextPatch.java
@@ -1,11 +1,18 @@
 package thePackmaster.patches;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.packs.AbstractCardPack;
+import thePackmaster.util.Wiz;
+import thePackmaster.ThePackmaster;
 
 import java.lang.reflect.Field;
 
@@ -32,6 +39,33 @@ public class SingleCardViewBorderTextPatch {
             AbstractCard card = (AbstractCard)cardField.get(__instance);
             if (card instanceof AbstractPackmasterCard) {
                 ((AbstractPackmasterCard)card).renderBorderText(sb, Settings.WIDTH / 2.0F, (float)yField.get(__instance), yOffsetBase, drawScale);
+            } else if (AbstractDungeon.player != null && AbstractDungeon.player.chosenClass == ThePackmaster.Enums.THE_PACKMASTER && card.getClass().getSuperclass().equals(AbstractCard.class)) {
+                AbstractCardPack pack = Wiz.getPackByCard(card);
+                if (pack != null) {
+                    float xPos, yPos, offsetY;
+                    BitmapFont font;
+                    String text = pack.name;
+                    if (text == null || card.isFlipped || card.isLocked || card.transparency <= 0.0F)
+                        return;
+                        font = FontHelper.cardTitleFont;
+                        xPos = Settings.WIDTH / 2.0F;
+                        yPos = (float)yField.get(__instance);
+                        offsetY = yOffsetBase * Settings.scale * drawScale / 2.0F;
+                    BitmapFont.BitmapFontData fontData = font.getData();
+                    float originalScale = fontData.scaleX;
+                    float scaleMulti = 0.8F;
+                    int length = text.length();
+                    if (length > 20) {
+                        scaleMulti -= 0.02F * (length - 20);
+                        if (scaleMulti < 0.5F)
+                            scaleMulti = 0.5F;
+                    }
+                    fontData.setScale(scaleMulti * (drawScale * 0.85f));
+                    Color color = AbstractPackmasterCard.packNameDisplayColor.cpy();
+                    color.a = card.transparency;
+                    FontHelper.renderRotatedText(sb, font, text, xPos, yPos, 0.0F, offsetY, card.angle, true, color);
+                    fontData.setScale(originalScale);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
