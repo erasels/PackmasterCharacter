448a9d7ad8fe4c8e9b3e6773d9f8ac28d4a6e5e9
modargo <modargo@gmail.com>
2023-01-25 17:34:15 -0500

    Content Creators pack: reduce Rhapsody to 125 (150) inspiration and 1 (2) draw

--- src/main/java/thePackmaster/cards/contentcreatorpack/Rhapsody.java
+++ src/main/java/thePackmaster/cards/contentcreatorpack/Rhapsody.java
@@ -14,15 +14,17 @@ public class Rhapsody extends AbstractContentCard {
     public final static String ID = makeID("Rhapsody");
     public Rhapsody() {
         super(ID, 2, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 150;
+        baseMagicNumber = magicNumber = 125;
+        baseSecondMagic = secondMagic = 1;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         applyToSelf(new InspirationPower(p, 1, magicNumber));
-        atb(new DrawCardAction(2));
+        atb(new DrawCardAction(this.secondMagic));
     }
 
     public void upp() {
-        upgradeMagicNumber(50);
+        upgradeMagicNumber(25);
+        upgradeSecondMagic(1);
     }
 }
