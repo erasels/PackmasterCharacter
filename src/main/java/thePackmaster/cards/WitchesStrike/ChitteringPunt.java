a9064bf24994f931fbd5d4131f000650d125bec1
erasels <emanuelbedburdick@yahoo.de>
2023-01-25 21:59:32 +0100

    Changes Chittering punt

--- src/main/java/thePackmaster/cards/WitchesStrike/ChitteringPunt.java
+++ src/main/java/thePackmaster/cards/WitchesStrike/ChitteringPunt.java
@@ -1,42 +1,37 @@
 package thePackmaster.cards.WitchesStrike;
 
-import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.cards.green.SuckerPunch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.actions.witchesstrikepack.ManifestAction;
-import thePackmaster.cardmodifiers.infestpack.InfestModifier;
-import thePackmaster.cards.infestpack.OnInfestCard;
 import thePackmaster.orbs.WitchesStrike.CrescentMoon;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
-public class ChitteringPunt extends AbstractWitchStrikeCard implements OnInfestCard {
+public class ChitteringPunt extends AbstractWitchStrikeCard {
     public final static String ID = makeID("ChitteringPunt");
     // intellij stuff attack, enemy, basic, 6, 3,  , , ,
 
     public ChitteringPunt() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = 7;
-        CardModifierManager.addModifier(this, new InfestModifier());
+        baseDamage = 9;
+        baseMagicNumber = magicNumber = 1;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        for (int i = 0; i < magicNumber; i++) {
+            addToBot(new ChannelAction(new CrescentMoon()));
+        }
     }
 
     public void upp() {
-        upgradeDamage(2);
+        upgradeDamage(3);
     }
 
     @Override
     public String cardArtCopy() {
         return SuckerPunch.ID;
-    }
-
-    @Override
-    public void onInfest(int infestCounter) {
-        addToBot(new ManifestAction(new CrescentMoon()));
     }
 }
