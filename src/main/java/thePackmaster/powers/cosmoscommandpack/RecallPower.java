cc3573245494db5057e2c8d74717b4b14e3d2a5d
ActuallyEnbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2023-01-27 18:44:59 +0000

    Nerfed Recall but also made it stackable
    Hopefully should make the power's true nature easier to understand for players

--- src/main/java/thePackmaster/powers/cosmoscommandpack/RecallPower.java
+++ src/main/java/thePackmaster/powers/cosmoscommandpack/RecallPower.java
@@ -1,11 +1,11 @@
 package thePackmaster.powers.cosmoscommandpack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.unique.ExhumeAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import thePackmaster.actions.cosmoscommandpack.ExhumeArbitraryNumberOfCardsAction;
 import thePackmaster.powers.AbstractPackmasterPower;
 import thePackmaster.powers.marisapack.AmplifyPowerHook;
 
@@ -16,26 +16,44 @@ public class RecallPower extends AbstractPackmasterPower implements AmplifyPower
     public static final String POWER_ID = makeID("RecallPower");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
+    public boolean usedThisTurn = false;
 
-    public RecallPower(AbstractCreature owner) {
-        super(POWER_ID, NAME, PowerType.BUFF, false, owner, 0);
+    public RecallPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        usedThisTurn = false;
+        updateDescription();
     }
 
     @Override
     public void onAmplify(AbstractCard c) {
-        atb(new AbstractGameAction() {
-            @Override
-            public void update() {
-                flash();
-                this.isDone = true;
-            }
-        });
-        atb(new WaitAction(0.1F));
-        atb(new ExhumeAction(false));
+        if (!usedThisTurn) {
+            atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    flash();
+                    this.isDone = true;
+                }
+            });
+            atb(new WaitAction(0.1F));
+            atb(new ExhumeArbitraryNumberOfCardsAction(amount));
+            usedThisTurn = true;
+            updateDescription();
+        }
     }
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0];
+        StringBuilder desc = new StringBuilder(DESCRIPTIONS[0]);
+        if (amount > 1)
+            desc.append(amount).append(DESCRIPTIONS[2]);
+        else
+            desc.append(DESCRIPTIONS[1]);
+        if (usedThisTurn)
+            desc.append(DESCRIPTIONS[3]);
+        description = desc.toString();
     }
 }
