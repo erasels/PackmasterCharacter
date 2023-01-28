a6afae2e8d4234b8c717f3030d9185c39c851a58
ActuallyEnbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2023-01-28 18:36:33 +0000

    Re-updated Recall + A bugfix for Cognition
    Balancing & consistency changes to Recall

--- src/main/java/thePackmaster/powers/cosmoscommandpack/RecallPower.java
+++ src/main/java/thePackmaster/powers/cosmoscommandpack/RecallPower.java
@@ -5,7 +5,7 @@ import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import thePackmaster.actions.cosmoscommandpack.ExhumeArbitraryNumberOfCardsAction;
+import thePackmaster.actions.cosmoscommandpack.ExhumeAndAddPurgeAction;
 import thePackmaster.powers.AbstractPackmasterPower;
 import thePackmaster.powers.marisapack.AmplifyPowerHook;
 
@@ -16,7 +16,7 @@ public class RecallPower extends AbstractPackmasterPower implements AmplifyPower
     public static final String POWER_ID = makeID("RecallPower");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
-    public boolean usedThisTurn = false;
+    public int usesThisTurn = 0;
 
     public RecallPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
@@ -24,13 +24,13 @@ public class RecallPower extends AbstractPackmasterPower implements AmplifyPower
 
     @Override
     public void atStartOfTurn() {
-        usedThisTurn = false;
+        usesThisTurn = 0;
         updateDescription();
     }
 
     @Override
     public void onAmplify(AbstractCard c) {
-        if (!usedThisTurn) {
+        if (usesThisTurn < amount) {
             atb(new AbstractGameAction() {
                 @Override
                 public void update() {
@@ -39,8 +39,8 @@ public class RecallPower extends AbstractPackmasterPower implements AmplifyPower
                 }
             });
             atb(new WaitAction(0.1F));
-            atb(new ExhumeArbitraryNumberOfCardsAction(amount));
-            usedThisTurn = true;
+            atb(new ExhumeAndAddPurgeAction());
+            usesThisTurn++;
             updateDescription();
         }
     }
@@ -49,11 +49,19 @@ public class RecallPower extends AbstractPackmasterPower implements AmplifyPower
     public void updateDescription() {
         StringBuilder desc = new StringBuilder(DESCRIPTIONS[0]);
         if (amount > 1)
-            desc.append(amount).append(DESCRIPTIONS[2]);
+            desc.append(DESCRIPTIONS[2]).append(amount).append(DESCRIPTIONS[3]);
         else
             desc.append(DESCRIPTIONS[1]);
-        if (usedThisTurn)
-            desc.append(DESCRIPTIONS[3]);
+        desc.append(DESCRIPTIONS[4]);
+        if (usesThisTurn == amount)
+            desc.append(DESCRIPTIONS[8]);
+        else if (usesThisTurn > 0) {
+            desc.append(DESCRIPTIONS[5]).append(usesThisTurn);
+            if (usesThisTurn == 1)
+                desc.append(DESCRIPTIONS[6]);
+            else
+                desc.append(DESCRIPTIONS[7]);
+        }
         description = desc.toString();
     }
 }
