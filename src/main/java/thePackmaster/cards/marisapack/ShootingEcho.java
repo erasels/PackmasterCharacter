d02f351714a9661f7f136f98074d6d54ba310b54
erasels <emanuelbedburdick@yahoo.de>
2023-02-03 21:00:15 +0100

    Nerf ShootingEcho to exhaust random

--- src/main/java/thePackmaster/cards/marisapack/ShootingEcho.java
+++ src/main/java/thePackmaster/cards/marisapack/ShootingEcho.java
@@ -1,17 +1,14 @@
 package thePackmaster.cards.marisapack;
 
-import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.actions.upgradespack.ExhaustRandomPredicateCardAction;
 import thePackmaster.util.Wiz;
 import thePackmaster.vfx.marisapack.BetterFireballEffect;
 import thePackmaster.vfx.marisapack.MissileStrikeEffect;
-
-import java.util.Locale;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -24,21 +21,14 @@ public class ShootingEcho extends AbstractMarisaCard {
         super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         damage = baseDamage = DMG;
         exhaust = true;
-
-        if (!SpireAnniversary5Mod.oneFrameMode)
-        setBackgroundTexture("anniv5Resources/images/512/marisapack/" + type.name().toLowerCase(Locale.ROOT) + ".png",
-                "anniv5Resources/images/1024/marisapack/" + type.name().toLowerCase(Locale.ROOT) + ".png");
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.vfx(new MissileStrikeEffect(m.hb.cX, m.hb.cY, BetterFireballEffect.randomFlareColor()), Settings.ACTION_DUR_FASTER);
         dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        Wiz.atb(new SelectCardsInHandAction(1, text, false, false, c -> true, list -> {
+        Wiz.atb(new ExhaustRandomPredicateCardAction(c->true, Wiz.hand(), c->{
             ShootingEcho.this.resetAttributes();
-            Wiz.makeInHand(ShootingEcho.this, list.size());
-
-            list.forEach(c -> Wiz.hand().moveToExhaustPile(c));
-            list.clear();
+            Wiz.makeInHand(ShootingEcho.this);
         }));
     }
 
