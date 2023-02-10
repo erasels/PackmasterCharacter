51e06482015a91994a43dc68b0fa427827cfb627
erasels <emanuelbedburdick@yahoo.de>
2023-02-10 17:37:56 +0100

    Fix SoilResiliency setting magicNumber

--- src/main/java/thePackmaster/cards/farmerpack/SoilResiliency.java
+++ src/main/java/thePackmaster/cards/farmerpack/SoilResiliency.java
@@ -2,46 +2,45 @@ package thePackmaster.cards.farmerpack;
 
 import com.evacipated.cardcrawl.mod.stslib.variables.ExhaustiveVariable;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
-
-import java.util.Iterator;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.atb;
 
 public class SoilResiliency extends AbstractFarmerCard {
     public final static String ID = makeID("SoilResiliency");
+
     public SoilResiliency() {
         super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
         baseMagicNumber = magicNumber = 1;
         ExhaustiveVariable.setBaseValue(this, 3);
     }
 
-
     public void triggerOnGlowCheck() {
         int count = checkTypes(false);
-        if (count >= 3) {this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();}
-        else{this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();}
+        if (count >= 3) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
         }
-
-
+    }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         atb(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
         int count = checkTypes(true);
-        if(count >= 3){
+        if (count >= 3) {
             baseMagicNumber += baseMagicNumber;
-            if(baseMagicNumber > 10){baseMagicNumber = 10;}
+            if (baseMagicNumber > 10) {
+                baseMagicNumber = 10;
+            }
+            magicNumber = baseMagicNumber;
         }
     }
 
     public void upp() {
-        ExhaustiveVariable.upgrade(this,1);
+        ExhaustiveVariable.upgrade(this, 1);
     }
 }
