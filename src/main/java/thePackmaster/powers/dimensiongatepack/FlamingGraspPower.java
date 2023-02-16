1fa045d95ac3511b7970fbf4667188c252b6ff5e
erasels <emanuelbedburdick@yahoo.de>
2023-02-16 21:25:47 +0100

    Fix Flaming Grasp not stacking

--- src/main/java/thePackmaster/powers/dimensiongatepack/FlamingGraspPower.java
+++ src/main/java/thePackmaster/powers/dimensiongatepack/FlamingGraspPower.java
@@ -1,13 +1,16 @@
 package thePackmaster.powers.dimensiongatepack;
 
+import com.evacipated.cardcrawl.mod.stslib.patches.NeutralPowertypePatch;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import thePackmaster.cards.dimensiongatepack2.FlamePillar;
 import thePackmaster.powers.AbstractPackmasterPower;
+import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.MAGIC;
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -18,7 +21,7 @@ public class FlamingGraspPower extends AbstractPackmasterPower implements Invisi
     public static final String DESCRIPTIONS[] = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
     public FlamingGraspPower(AbstractCreature owner, int amount) {
-        super(POWER_ID, NAME, PowerType.BUFF, true, owner, amount);
+        super(POWER_ID, NAME, NeutralPowertypePatch.NEUTRAL, false, owner, amount);
 
     }
 
@@ -28,7 +31,7 @@ public class FlamingGraspPower extends AbstractPackmasterPower implements Invisi
             for (int i = 0; i < amount; i++) {
                 addToBot(new MakeTempCardInHandAction(new FlamePillar()));
             }
-            removeThisInvisibly();
+            Wiz.atb(new ReducePowerAction(owner, owner, this, amount));
         }
     }
 }
