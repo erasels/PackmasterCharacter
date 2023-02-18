c04d9f45cc921ed93604d80f302151f2b8ba68fd
Max Swietnicki <swietnickimax@gmail.com>
2023-02-18 14:10:44 -0500

    2

--- src/main/java/thePackmaster/cards/serpentinepack/DaggerRush.java
+++ src/main/java/thePackmaster/cards/serpentinepack/DaggerRush.java
@@ -13,7 +13,7 @@ public class DaggerRush extends AbstractSerpentineCard {
 
     private static final int COST = 1;
     private static final int MAGIC = 3;
-    private static final int DAMAGE = 3;
+    private static final int DAMAGE = 2;
     private static final int UPGRADE_PLUS_DAMAGE = 1;
     public final static String ID = makeID("DaggerRush");
 
