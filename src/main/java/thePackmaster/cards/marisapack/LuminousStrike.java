fca748a89f479d3be5f5902679fe001ce3af01d5
erasels <emanuelbedburdick@yahoo.de>
2023-02-19 20:44:00 +0100

    Rework Luminous Strike

--- src/main/java/thePackmaster/cards/marisapack/LuminousStrike.java
+++ src/main/java/thePackmaster/cards/marisapack/LuminousStrike.java
@@ -3,29 +3,24 @@ package thePackmaster.cards.marisapack;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import com.megacrit.cardcrawl.vfx.StarBounceEffect;
-import thePackmaster.SpireAnniversary5Mod;
-import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.powers.marisapack.ChargeUpPower;
 import thePackmaster.util.Wiz;
 import thePackmaster.vfx.marisapack.CasualFlameParticleEffect;
 import thePackmaster.vfx.marisapack.FireIgniteEffect;
-
-import java.util.Locale;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
 public class LuminousStrike extends AbstractMarisaCard implements AmplifyCard{
     public final static String ID = makeID(LuminousStrike.class.getSimpleName());
-    private static final int DMG = 5, UPG_DMG = 4;
-    private static final int MAGIC = 4;
+    private static final int DMG = 5, UPG_DMG = 2;
+    private static final int MAGIC = 5, UPG_MAGIC = 1;
 
     public LuminousStrike() {
         super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         damage = baseDamage = DMG;
         baseMagicNumber = magicNumber = MAGIC;
         tags.add(CardTags.STRIKE);
-
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -37,6 +32,7 @@ public class LuminousStrike extends AbstractMarisaCard implements AmplifyCard{
 
     public void upp() {
         upgradeDamage(UPG_DMG);
+        upgradeMagicNumber(UPG_MAGIC);
     }
 
     @Override
@@ -48,34 +44,11 @@ public class LuminousStrike extends AbstractMarisaCard implements AmplifyCard{
     public void useAmplified(AbstractPlayer p, AbstractMonster m) {
         Wiz.vfx(new FireIgniteEffect(m.hb.cX, m.hb.cY, damage));
         Wiz.vfx(new CasualFlameParticleEffect(m.hb.cX, m.hb.cY));
+        Wiz.applyToSelf(new ChargeUpPower(magicNumber));
     }
 
     @Override
     public int getAmplifyCost() {
-        return 2;
-    }
-
-    @Override
-    public void applyPowers() {
-        boolean amp = shouldAmplify(this);
-        int bd = baseDamage;
-        if(amp) {
-            baseDamage = baseDamage + EnergyPanel.totalCount * magicNumber;
-        }
-        super.applyPowers();
-        if(amp) isDamageModified = true;
-        baseDamage = bd;
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster mo) {
-        boolean amp = shouldAmplify(this);
-        int bd = baseDamage;
-        if(amp) {
-            baseDamage = baseDamage + EnergyPanel.totalCount * magicNumber;
-        }
-        super.calculateCardDamage(mo);
-        if(amp) isDamageModified = true;
-        baseDamage = bd;
+        return 1;
     }
 }
