e275ca5c1fecb654ede01251445c31d21a853f16
Mwallx <89969909+Mwallx@users.noreply.github.com>
2023-02-22 11:12:24 +0800

    routine zhs polish

--- src/main/java/thePackmaster/cards/distortionpack/Static.java
+++ src/main/java/thePackmaster/cards/distortionpack/Static.java
@@ -1,9 +1,9 @@
 package thePackmaster.cards.distortionpack;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.actions.distortionpack.StaticAction;
-import thePackmaster.cards.AbstractPackmasterCard;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.atb;
@@ -25,5 +25,15 @@ public class Static extends AbstractDistortionCard {
 
     public void upp() {
         upgradeMagicNumber(1);
+    }
+
+    @Override //zhs card text thing
+    public void initializeDescriptionCN() {
+        super.initializeDescriptionCN();
+        if (Settings.language == Settings.GameLanguage.ZHS) {
+            StringBuilder first_line = new StringBuilder();
+            this.description.get(0).text = first_line.append(this.description.get(0).text).append("。").toString();
+            this.description.remove(1);
+        }
     }
 }
