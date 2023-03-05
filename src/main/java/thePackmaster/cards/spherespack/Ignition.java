d5434e2a7862d69368822ae58e147e1c44b4f265
modargo <modargo@gmail.com>
2023-03-05 10:58:02 -0500

    Spheres pack: initial files and Ignition

---
+++ src/main/java/thePackmaster/cards/spherespack/Ignition.java
@@ -0,0 +1,37 @@
+package thePackmaster.cards.spherespack;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.orbs.spherespack.Blaze;
+import thePackmaster.orbs.spherespack.Hope;
+import thePackmaster.orbs.spherespack.Snow;
+
+public class Ignition extends AbstractSpheresCard {
+    public static final String ID = SpireAnniversary5Mod.makeID("Ignition");
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_DAMAGE = 3;
+
+    public Ignition() {
+        super(ID, COST, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        this.baseDamage = DAMAGE;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void upp() {
+        this.upgradeDamage(UPGRADE_DAMAGE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new ChannelAction(new Blaze()));
+        this.addToBot(new ChannelAction(new Snow()));
+        this.addToBot(new ChannelAction(new Hope()));
+    }
+}
---
+++ src/main/java/thePackmaster/packs/SpheresPack.java
@@ -0,0 +1,28 @@
+package thePackmaster.packs;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.cards.spherespack.Ignition;
+
+import java.util.ArrayList;
+
+public class SpheresPack extends AbstractCardPack {
+    public static final String ID = SpireAnniversary5Mod.makeID("SpheresPack");
+    private static final UIStrings UI_STRINGS = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String NAME = UI_STRINGS.TEXT[0];
+    public static final String DESC = UI_STRINGS.TEXT[1];
+    public static final String AUTHOR = UI_STRINGS.TEXT[2];
+    public static final String CREDITS = UI_STRINGS.TEXT[3];
+
+    public SpheresPack() {
+        super(ID, NAME, DESC, AUTHOR, CREDITS);
+    }
+
+    @Override
+    public ArrayList<String> getCards() {
+        ArrayList<String> cards = new ArrayList<>();
+        cards.add(Ignition.ID);
+        return cards;
+    }
+}
