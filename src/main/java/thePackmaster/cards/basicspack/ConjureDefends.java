d6e89d01b7388625f5317d58dfbf0683a699bd4b
Jack Renoson <zombiekiller.jayjay@gmail.com>
2023-03-05 16:34:00 +0100

    ConjureDefends's defends plays X times instead of defend+ X additional times

--- src/main/java/thePackmaster/cards/basicspack/ConjureDefends.java
+++ src/main/java/thePackmaster/cards/basicspack/ConjureDefends.java
@@ -21,19 +21,17 @@ public class ConjureDefends extends AbstractPackmasterCard {
         super(ID, -1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF, "basics");
         this.exhaust = true;
         this.cardsToPreview = new Defend();
-        this.cardsToPreview.upgrade();
-        this.cardsToPreview.rawDescription += CardCrawlGame.languagePack.getUIString(SpireAnniversary5Mod.makeID("DuplicateModifier")).TEXT[3];
+        this.cardsToPreview.rawDescription += CardCrawlGame.languagePack.getUIString(SpireAnniversary5Mod.makeID("DuplicateModifier")).TEXT[2];
         this.cardsToPreview.initializeDescription();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractCard card = this.cardsToPreview.makeStatEquivalentCopy();
-        if (this.upgraded) {
-            addToBot(new ConjureCardsAction(p, this.freeToPlayOnce, this.energyOnUse + 1, card));
-        } else addToBot(new ConjureCardsAction(p, this.freeToPlayOnce, this.energyOnUse, card));
+        AbstractCard card = this.cardsToPreview.makeStatEquivalentCopy();addToBot(new ConjureCardsAction(p, this.freeToPlayOnce, this.energyOnUse, card));
         addToBot(new RefundAction(this, 1));
     }
 
-    public void upp(){}
+    public void upp(){
+        this.cardsToPreview.upgrade();
+    }
 }
