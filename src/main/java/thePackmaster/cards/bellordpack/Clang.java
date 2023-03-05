086e13be681b135fc15259e3697836f312b55ac8
erasels <emanuelbedburdick@yahoo.de>
2023-03-05 20:51:01 +0100

    Remove minimum Block from Petra

--- src/main/java/thePackmaster/cards/bellordpack/Clang.java
+++ src/main/java/thePackmaster/cards/bellordpack/Clang.java
@@ -5,7 +5,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.ClashEffect;
-import thePackmaster.cards.AbstractPackmasterCard;
 
 import static com.megacrit.cardcrawl.cards.AbstractCard.CardColor.CURSE;
 import static thePackmaster.SpireAnniversary5Mod.makeID;
