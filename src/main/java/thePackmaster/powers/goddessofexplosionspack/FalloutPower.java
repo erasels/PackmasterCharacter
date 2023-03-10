da27ab103265eb1d78f769e97f3f654fe396d8f1
AlexMdle <92187869+AlexMdle@users.noreply.github.com>
2023-03-10 04:40:30 +0100

    - chain reaction change

--- src/main/java/thePackmaster/powers/goddessofexplosionspack/FalloutPower.java
+++ src/main/java/thePackmaster/powers/goddessofexplosionspack/FalloutPower.java
@@ -13,12 +13,12 @@ import thePackmaster.util.Wiz;
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
 
-public class ChainReactionPower extends AbstractPackmasterPower {
-    public static final String POWER_ID = makeID("ChainReactionPower");
+public class FalloutPower extends AbstractPackmasterPower {
+    public static final String POWER_ID = makeID("FalloutPower");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String DESCRIPTIONS[] = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
-    public ChainReactionPower(AbstractCreature owner, int amount) {
+    public FalloutPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
     }
 
