e940e3ac90860db46216ab68cb47893183a22387
modargo <modargo@gmail.com>
2023-03-09 23:47:38 -0500

    Spheres pack: make Winterwisp glow when you have an empty orb slot

--- src/main/java/thePackmaster/cards/spherespack/Winterwisp.java
+++ src/main/java/thePackmaster/cards/spherespack/Winterwisp.java
@@ -3,8 +3,10 @@ package thePackmaster.cards.spherespack;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import thePackmaster.SpireAnniversary5Mod;
@@ -47,5 +49,13 @@ public class Winterwisp extends AbstractSpheresCard {
                 this.isDone = true;
             }
         });
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (AbstractDungeon.player.hasEmptyOrb()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        }
     }
 }
