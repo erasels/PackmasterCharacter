ebb289009414d4f1cf7695d85615eee48dc582f4
AlexMdle <92187869+AlexMdle@users.noreply.github.com>
2023-03-12 11:02:00 +0100

    - atomic piledriver change

--- src/main/java/thePackmaster/cards/goddessofexplosionspack/AtomicPiledriver.java
+++ src/main/java/thePackmaster/cards/goddessofexplosionspack/AtomicPiledriver.java
@@ -3,6 +3,7 @@ package thePackmaster.cards.goddessofexplosionspack;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -11,6 +12,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
 import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
+import thePackmaster.powers.goddessofexplosionspack.FalloutPower;
 import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -18,12 +20,15 @@ import static thePackmaster.SpireAnniversary5Mod.makeID;
 public class AtomicPiledriver extends AbstractGoddessOfExplosionsCard {
     public final static String ID = makeID("AtomicPiledriver");
 
-    private static final int DAMAGE = 24;
-    private static final int DAMAGE_UP = 6;
+    private static final int DAMAGE = 20;
+    private static final int DAMAGE_UP = 4;
+    private static final int MAGIC = 8;
+    private static final int MAGIC_UP = 2;
 
     public AtomicPiledriver() {
         super(ID, 2, CardType.ATTACK, CardRarity.SPECIAL, CardTarget.ENEMY);
         baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = MAGIC;
         isEthereal = true;
         exhaust = true;
     }
@@ -32,12 +37,14 @@ public class AtomicPiledriver extends AbstractGoddessOfExplosionsCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.RED, true));
         CardCrawlGame.sound.play("STANCE_ENTER_WRATH");
-        Wiz.atb(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
         Wiz.atb(new VFXAction(new ExplosionSmallEffect(m.hb.cX, m.hb.cY), 0.1F));
+        Wiz.atb(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        Wiz.atb(new ApplyPowerAction(m,p, new FalloutPower(m,magicNumber),magicNumber,true));
     }
 
     @Override
     public void upp() {
-            upgradeDamage(DAMAGE_UP);
+        upgradeDamage(DAMAGE_UP);
+        upgradeMagicNumber(MAGIC_UP);
     }
 }
