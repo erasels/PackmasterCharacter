a5d254009b14f801bcab715542f271a00fb13047
AlexMdle <92187869+AlexMdle@users.noreply.github.com>
2023-03-12 22:40:50 +0100

    - goddess of explosions tweaks

--- src/main/java/thePackmaster/powers/goddessofexplosionspack/EveOfDestructionPower.java
+++ src/main/java/thePackmaster/powers/goddessofexplosionspack/EveOfDestructionPower.java
@@ -1,11 +1,8 @@
 package thePackmaster.powers.goddessofexplosionspack;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.powers.TheBombPower;
 import thePackmaster.powers.AbstractPackmasterPower;
-import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -20,17 +17,7 @@ public class EveOfDestructionPower extends AbstractPackmasterPower {
     }
 
     @Override
-    public void atStartOfTurn() {
-        for(int i = 0; i < amount; i++)
-        Wiz.applyToSelf(new TheBombPower(owner, 3, 40));
-        flash();
-    }
-
-    @Override
     public void updateDescription() {
-        if (amount == 1)
-            description = DESCRIPTIONS[0];
-        else
-            description = DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+        description = DESCRIPTIONS[0] + amount*100 + DESCRIPTIONS[1];
     }
 }
