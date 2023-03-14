dfb7294942329061de2d940dab622df47d55c5d5
Dark Vexon <edanvirgo@gmail.com>
2023-03-13 17:40:58 -0700

    ARRRRRGHHH

--- src/main/java/thePackmaster/cards/jockeypack/Giddyap.java
+++ src/main/java/thePackmaster/cards/jockeypack/Giddyap.java
@@ -1,11 +1,13 @@
 package thePackmaster.cards.jockeypack;
 
+import com.megacrit.cardcrawl.actions.animations.AnimateJumpAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.powers.jockeypack.GiddyapPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.applyToSelf;
+import static thePackmaster.util.Wiz.atb;
 
 public class Giddyap extends AbstractJockeyCard {
     public final static String ID = makeID("Giddyap");
@@ -17,6 +19,7 @@ public class Giddyap extends AbstractJockeyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new AnimateJumpAction(p));
         applyToSelf(new GiddyapPower());
     }
 
