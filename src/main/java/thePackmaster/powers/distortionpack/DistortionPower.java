767f8c094198316be6ef4da005ede2577df8e461
erasels <emanuelbedburdick@yahoo.de>
2023-03-19 19:05:11 +0100

    Distortion damage source changed

--- src/main/java/thePackmaster/powers/distortionpack/DistortionPower.java
+++ src/main/java/thePackmaster/powers/distortionpack/DistortionPower.java
@@ -17,6 +17,7 @@ import com.megacrit.cardcrawl.vfx.combat.FlashPowerEffect;
 import com.megacrit.cardcrawl.vfx.combat.GainPowerEffect;
 import thePackmaster.patches.distortionpack.EnemyOnExhaustPatch;
 import thePackmaster.powers.AbstractPackmasterPower;
+import thePackmaster.util.Wiz;
 import thePackmaster.vfx.distortionpack.PixelEffect;
 
 import java.util.ArrayList;
@@ -49,7 +50,7 @@ public class DistortionPower extends AbstractPackmasterPower implements EnemyOnE
                 this.isDone = true;
             }
         });
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(this.owner, new DamageInfo(this.owner, this.amount, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.NONE, true));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(this.owner, new DamageInfo(Wiz.p(), this.amount, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.NONE, true));
     }
 
     @Override
