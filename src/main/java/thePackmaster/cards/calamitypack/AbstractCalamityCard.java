e7b86eb9e24b73a567041065471bb6f92b152a80
modargo <modargo@gmail.com>
2023-03-23 18:50:00 -0400

    Calamity pack: use the Shaman card back for now

--- src/main/java/thePackmaster/cards/calamitypack/AbstractCalamityCard.java
+++ src/main/java/thePackmaster/cards/calamitypack/AbstractCalamityCard.java
@@ -6,7 +6,7 @@ public abstract class AbstractCalamityCard extends AbstractPackmasterCard
 {
     public AbstractCalamityCard(String cardID, int cost, CardType type, CardRarity rarity, CardTarget target)
     {
-        super(cardID, cost, type, rarity, target, (String)null);
+        super(cardID, cost, type, rarity, target, "shaman"); // Temporary -- will be switched to its own card back when ready
     }
 
     @Override
