8afe00c24482e0f407977dd5e3e4201e6415f2e0
Dark Vexon <edanvirgo@gmail.com>
2023-03-23 16:26:17 -0700

    Funnier name for Reality Master, and actually Ghost can probably be buffed

--- src/main/java/thePackmaster/cards/colorlesspack/Ghost.java
+++ src/main/java/thePackmaster/cards/colorlesspack/Ghost.java
@@ -28,7 +28,7 @@ public class Ghost extends AbstractColorlessPackCard implements StartupCard {
 
     public Ghost() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 10;
+        baseMagicNumber = magicNumber = 12;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
