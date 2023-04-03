aba0220ca7f10e03b8c74ebbc81420e7712235ed
Alchyr <isithalchyr@gmail.com>
2023-04-03 02:44:42 -0700

    icon

--- src/main/java/thePackmaster/powers/instadeathpack/LockPower.java
+++ src/main/java/thePackmaster/powers/instadeathpack/LockPower.java
@@ -1,6 +1,13 @@
 package thePackmaster.powers.instadeathpack;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.mod.stslib.patches.ExtraIconsPatch;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.evacipated.cardcrawl.mod.stslib.util.extraicons.ExtraIcons;
+import com.evacipated.cardcrawl.mod.stslib.util.extraicons.IconPayload;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
@@ -12,6 +19,9 @@ import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.powers.AbstractPackmasterPower;
+import thePackmaster.util.TexLoader;
+
+import java.util.ArrayList;
 
 
 public class LockPower extends AbstractPackmasterPower implements NonStackablePower {
@@ -19,7 +29,11 @@ public class LockPower extends AbstractPackmasterPower implements NonStackablePo
     private static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     private static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
+    private static final Texture t = TexLoader.getTexture(SpireAnniversary5Mod.modID + "Resources/images/powers/LockPower84.png");
+
     public AbstractCard card;
+
+    private IconPayload icon;
 
     public LockPower(final AbstractCreature owner, AbstractCard c, int amount)
     {
@@ -27,6 +41,8 @@ public class LockPower extends AbstractPackmasterPower implements NonStackablePo
 
         this.card = c;
         updateDescription();
+
+        icon = new IconPayload(ExtraIcons.icon(t).text(String.valueOf(amount)).textOffsetY(-13f));
     }
 
     @Override
@@ -52,6 +68,23 @@ public class LockPower extends AbstractPackmasterPower implements NonStackablePo
         {
             this.flash();
             this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        }
+    }
+
+    @Override
+    public void renderIcons(SpriteBatch sb, float x, float y, Color c) {
+        super.renderIcons(sb, x, y, c);
+
+        if (card != null) {
+            if (!icon.getText().equals(String.valueOf(amount)))
+                ReflectionHacks.setPrivate(icon, IconPayload.class, "text", String.valueOf(amount));
+
+            //To avoid unnecessarily filling the list as it isn't cleared if the card isn't rendered.
+            //And also because I didn't want to make a separate cardmod which could maybe somehow get desynced from the power.
+            ArrayList<IconPayload> icons = ExtraIconsPatch.ExtraIconsField.extraIcons.get(card);
+            if (!icons.contains(icon)) {
+                icons.add(icon);
+            }
         }
     }
 
