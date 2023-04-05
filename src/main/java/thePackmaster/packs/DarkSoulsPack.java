bfcdcd4cc73a858cae36a17d186bf5883279986e
bhock1 <gz21472@bristol.ac.uk>
2023-04-05 16:02:39 +0100

    added Cardstrings.json

---
+++ src/main/java/thePackmaster/packs/DarkSoulsPack.java
@@ -0,0 +1,26 @@
+package thePackmaster.packs;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.cards.darksoulspack;
+
+import java.util.ArrayList;
+
+public class DarkSoulsPack extends AbstractCardPack {
+    public static final String ID = SpireAnniversary5Mod.makeID("DarkSoulsPack");
+    private static final UIStrings UI_STRINGS = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String NAME = UI_STRINGS.TEXT[0];
+    public static final String DESC = UI_STRINGS.TEXT[1];
+    public static final String AUTHOR = UI_STRINGS.TEXT[2];
+
+    public DarkSoulsPack() {
+        super(ID, NAME, DESC, AUTHOR);
+    }
+
+    @Override
+    public ArrayList<String> getCards() {
+        ArrayList<String> cards = new ArrayList<>();
+        return cards;
+    }
+}
