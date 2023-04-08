850c79a1f9334f9c16fe3bb150843da317475534
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2023-04-08 21:06:35 +0100

    duo heal

---
+++ src/main/java/thePackmaster/cards/summonerspellspack/DuoHealCard.java
@@ -0,0 +1,38 @@
+package thePackmaster.cards.summonerspellspack;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.actions.summonerspellspack.ClutchTeleportAction;
+import thePackmaster.powers.summonerspellspack.GhostedPower;
+
+public class DuoHealCard extends AbstractSummonerSpellsCard {
+    public static final String ID = SpireAnniversary5Mod.makeID("DuoHeal");
+    private static final int COST = 1;
+    private static final int MAGIC = 7;
+    private static final int UPG_MAGIC = 3;
+    private static final int MAGIC2 = 1;
+
+
+    public DuoHealCard() {
+        super(ID, COST, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        exhaust = true;
+        magicNumber = baseMagicNumber = MAGIC;
+        secondMagic = baseSecondMagic = MAGIC2;
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new HealAction(p, p, this.magicNumber, 0));
+        addToBot(new HealAction(m, p, this.magicNumber, 1));
+
+        addToBot(new ApplyPowerAction(p, p, new GhostedPower(p, this.secondMagic), this.secondMagic));
+    }
+}
---
+++ src/main/java/thePackmaster/powers/summonerspellspack/GhostedPower.java
@@ -0,0 +1,49 @@
+package thePackmaster.powers.summonerspellspack;
+
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import thePackmaster.powers.AbstractPackmasterPower;
+import thePackmaster.util.Wiz;
+
+import static thePackmaster.SpireAnniversary5Mod.makeID;
+
+public class GhostedPower extends AbstractPackmasterPower {
+    public static final String POWER_ID = makeID("GhostedPower");
+    public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
+    public static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
+
+    public GhostedPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, NAME, PowerType.BUFF, true, owner, 1);
+        this.amount = amount;
+    }
+
+    @Override
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        if (target.currentHealth <= damageAmount) {
+            addToBot(new ApplyPowerAction(Wiz.p(), Wiz.p(), new GhostedPower(Wiz.p(), 1), 1));
+        }
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        this.addToBot(new DrawCardAction(this.amount));
+
+        if (this.amount > 1)
+            this.addToBot(new ReducePowerAction(this.owner, this.owner, "GhostedPower", 1));
+        else
+            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, "GhostedPower"));
+    }
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+    }
+
+    public void updateDescription() {
+        this.description = this.amount == 1 ?
+                DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1] :
+                DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
+    }
+
+}
