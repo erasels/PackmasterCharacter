0ce869bd4d6f041332b222f1183cd06c17fa43be
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2023-04-09 21:20:02 +0100

    Cleansing fountain

---
+++ src/main/java/thePackmaster/cards/summonerspellspack/CleansingFountainCard.java
@@ -0,0 +1,38 @@
+package thePackmaster.cards.summonerspellspack;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.actions.summonerspellspack.SpiritBarrierAction;
+import thePackmaster.powers.summonerspellspack.CleansingFountainPower;
+import thePackmaster.powers.summonerspellspack.GhostedPower;
+
+public class CleansingFountainCard extends AbstractSummonerSpellsCard {
+    public static final String ID = SpireAnniversary5Mod.makeID("CleansingFountain");
+    private static final int COST = 2;
+    private static final int MAGIC = 1;
+    private static final int MAGIC2 = 2;
+    private static final int UPG_MAGIC2 = 97;
+
+
+    public CleansingFountainCard() {
+        super(ID, COST, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
+        magicNumber = baseMagicNumber = MAGIC;
+        secondMagic = baseSecondMagic = MAGIC2;
+    }
+
+    @Override
+    public void upp() {
+        upgradeSecondMagic(UPG_MAGIC2);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new RemoveDebuffsAction(p));
+        addToBot(new ApplyPowerAction(p, p, new ArtifactPower(p, magicNumber), magicNumber));
+        addToBot(new ApplyPowerAction(p, p, new CleansingFountainPower(p, secondMagic), secondMagic));
+    }
+}
