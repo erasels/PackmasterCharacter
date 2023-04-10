07107827b9dceaffc84ce50d01667128552ce4cb
Jedi515 <44167564+Jedi515@users.noreply.github.com>
2023-04-10 20:46:30 +1000

    Update TunnelVision.java
    I definitely didn't forget to have it apply lock-on when upgraded.

--- src/main/java/thePackmaster/cards/lockonpack/TunnelVision.java
+++ src/main/java/thePackmaster/cards/lockonpack/TunnelVision.java
@@ -3,7 +3,9 @@ package thePackmaster.cards.lockonpack;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
 import thePackmaster.powers.lockonpack.TunnelVisionPower;
+import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -23,6 +25,10 @@ public class TunnelVision extends AbstractLockonCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (magicNumber > 0)
+        {
+            for (AbstractMonster mon : Wiz.getEnemies()) addToBot(new ApplyPowerAction(mon, p, new LockOnPower(mon, magicNumber)));
+        }
         addToBot(new ApplyPowerAction(p, p, new TunnelVisionPower(p)));
     }
 }
