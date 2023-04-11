c25a2724c8d11516617760dc386d28813da12026
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2023-04-11 20:58:52 +0100

    TP - add random summoner spells pack cards to your hand

--- src/main/java/thePackmaster/cards/summonerspellspack/ClutchTeleport.java
+++ src/main/java/thePackmaster/cards/summonerspellspack/ClutchTeleport.java
@@ -1,27 +1,44 @@
 package thePackmaster.cards.summonerspellspack;
 
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.actions.FlexibleDiscoveryAction;
 import thePackmaster.actions.summonerspellspack.ClutchTeleportAction;
+import thePackmaster.packs.AbstractCardPack;
+import thePackmaster.packs.SummonerSpellsPack;
+
+import java.util.ArrayList;
 
 public class ClutchTeleport extends AbstractSummonerSpellsCard {
     public static final String ID = SpireAnniversary5Mod.makeID("ClutchTeleport");
-    private static final int COST = 1;
-    private static final int UPG_COST = 0;
+    private static final int COST = 3;
+    private static final int MAGIC = 3;
+    private static final int UPG_MAGIC = 2;
 
     public ClutchTeleport() {
         super(ID, COST, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
         exhaust = true;
+        magicNumber = baseMagicNumber = MAGIC;
     }
 
     @Override
     public void upp() {
-        upgradeBaseCost(UPG_COST);
+        upgradeMagicNumber(UPG_MAGIC);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ClutchTeleportAction());
+        ArrayList<AbstractCard> packCards = new ArrayList<>();
+
+        for (int i = 0; i < this.magicNumber; i++) {
+            AbstractCard c = SpireAnniversary5Mod.getRandomCardFromPack(new SummonerSpellsPack()).makeStatEquivalentCopy();
+            c.setCostForTurn(0);
+            addToBot(new MakeTempCardInHandAction(c, 1));
+        }
+
     }
 }
