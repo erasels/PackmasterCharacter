828c7d12f62927f5af44045f3948c129d1eff5ba
erasels <emanuelbedburdick@yahoo.de>
2023-04-18 13:32:44 +0200

    Change Spreading Spores

--- src/main/java/thePackmaster/cards/dimensiongatepack3/SpreadingSpores.java
+++ src/main/java/thePackmaster/cards/dimensiongatepack3/SpreadingSpores.java
@@ -1,17 +1,13 @@
 package thePackmaster.cards.dimensiongatepack3;
 
 import basemod.helpers.TooltipInfo;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.ThornsPower;
 import thePackmaster.cards.dimensiongateabstracts.AbstractDimensionalCardTrain;
-import thePackmaster.powers.dimensiongate3pack.TempHPRegenPower;
 import thePackmaster.util.Wiz;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -22,22 +18,13 @@ public class SpreadingSpores extends AbstractDimensionalCardTrain {
 
     public SpreadingSpores() {
         super(ID, 1, CardRarity.UNCOMMON, CardType.POWER, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 2;
+        baseMagicNumber = magicNumber = 3;
         isEthereal = true;
-    }
-
-    @Override
-    public List<TooltipInfo> getCustomTooltips() {
-        if(ablation == null) {
-            AbstractPower p = new TempHPRegenPower(null, 999);
-            ablation = new TooltipInfo(p.name, p.description.replace("999", "X"));
-        }
-        return Arrays.asList(ablation);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.applyToSelf(new ThornsPower(p, magicNumber));
-        Wiz.applyToSelf(new TempHPRegenPower(p, magicNumber));
+        Wiz.atb(new AddTemporaryHPAction(p, p, magicNumber));
         Wiz.atb(new MakeTempCardInDrawPileAction(this.makeStatEquivalentCopy(), 1, true, true));
     }
 
