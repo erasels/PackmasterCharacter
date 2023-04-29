db8fcca2ee1db317ba28d1f7082b00e692b2207b
Max Swietnicki <swietnickimax@gmail.com>
2023-04-29 15:00:15 -0400

    cleanup

--- src/main/java/thePackmaster/cards/serpentinepack/Patience.java
+++ src/main/java/thePackmaster/cards/serpentinepack/Patience.java
@@ -1,13 +1,11 @@
 package thePackmaster.cards.serpentinepack;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import thePackmaster.stances.serpentinepack.CunningStance;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
