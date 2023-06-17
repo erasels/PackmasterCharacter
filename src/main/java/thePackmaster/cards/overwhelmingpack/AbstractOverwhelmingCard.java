7bc463a150e5aaaa9ed71f37c84baf7ad7b0d7ea
Alchyr <isithalchyr@gmail.com>
2023-06-17 01:32:54 -0700

    more images, ratings, hat text

--- src/main/java/thePackmaster/cards/overwhelmingpack/AbstractOverwhelmingCard.java
+++ src/main/java/thePackmaster/cards/overwhelmingpack/AbstractOverwhelmingCard.java
@@ -4,6 +4,6 @@ import thePackmaster.cards.AbstractPackmasterCard;
 
 public abstract class AbstractOverwhelmingCard extends AbstractPackmasterCard {
     public AbstractOverwhelmingCard(String cardID, int cost, CardType type, CardRarity rarity, CardTarget target) {
-        super(cardID, cost, type, rarity, target, (String) null);
+        super(cardID, cost, type, rarity, target, "overwhelming");
     }
 }
