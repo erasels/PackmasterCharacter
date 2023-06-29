b6be5dda87444f2f16be4d92974315a5df370ceb
erasels <emanuelbedburdick@yahoo.de>
2023-06-29 09:50:49 +0200

    Move ColorlessCreations Pack

--- src/main/java/thePackmaster/cards/colorlesspack/AbstractColorlessPackCard.java
+++
@@ -1,9 +0,0 @@
-package thePackmaster.cards.colorlesspack;
-
-import thePackmaster.cards.AbstractPackmasterCard;
-
-public abstract class AbstractColorlessPackCard extends AbstractPackmasterCard {
-    public AbstractColorlessPackCard(String cardID, int cost, CardType type, CardRarity rarity, CardTarget target) {
-        super(cardID, cost, type, rarity, target, "colorless");
-    }
-}
