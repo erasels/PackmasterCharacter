a56d5c3134c2ec5e7ecafdd22432da5abfc155a1
erasels <emanuelbedburdick@yahoo.de>
2023-07-02 23:24:34 +0200

    Add HoverText and Buton functionality

--- src/main/java/thePackmaster/ui/MainMenuExpansionPacksButton.java
+++ src/main/java/thePackmaster/ui/MainMenuExpansionPacksButton.java
@@ -1,11 +1,16 @@
 package thePackmaster.ui;
 
+import basemod.ReflectionHacks;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.evacipated.cardcrawl.modthespire.Loader;
+import com.evacipated.cardcrawl.modthespire.ModInfo;
+import com.evacipated.cardcrawl.modthespire.ModList;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.TipHelper;
 import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
 import com.megacrit.cardcrawl.ui.buttons.Button;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
@@ -13,10 +18,12 @@ import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.util.TexLoader;
 import thePackmaster.vfx.VictoryConfettiEffect;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class MainMenuExpansionPacksButton extends Button {
-    public static final Texture tex = TexLoader.getTexture(SpireAnniversary5Mod.makeImagePath("ui/CardTextInfestIcon.png"));
+    public static final Texture tex = TexLoader.getTexture(SpireAnniversary5Mod.makeImagePath("ui/MMReminderBtn.png"));
     private static final float PARTICLE_CD = 0.65f;
     private float particleTimer = PARTICLE_CD;
     private ArrayList<AbstractGameEffect> effects = new ArrayList<>();
@@ -32,6 +39,9 @@ public class MainMenuExpansionPacksButton extends Button {
 
         super.update();
 
+        if(pressed)
+            return;
+
         this.particleTimer -= Gdx.graphics.getDeltaTime();
         if (this.particleTimer < 0.0F) {
             this.particleTimer = PARTICLE_CD;
@@ -44,10 +54,37 @@ public class MainMenuExpansionPacksButton extends Button {
 
         effects.forEach(AbstractGameEffect::update);
 
-        if(pressed) {
-            pressed = false;
+        if(hb.hovered) {
+            TipHelper.renderGenericTip((x - tex.getWidth()) - (20f * Settings.xScale), y, "NOW!!!?!", "Enable expansion packs, fucko!");
+        }
 
-            //Enable mod, show that it's enabled remind them to restart and remove this button
+        if(pressed) {
+            ModInfo[] allMods = ReflectionHacks.getPrivateStatic(Loader.class, "ALLMODINFOS");
+            boolean found = false;
+            for(ModInfo m : allMods) {
+                if(m.ID.equals(SpireAnniversary5Mod.expansionPackModID)) {
+                    found = true;
+                    ArrayList<File> newModList = new ArrayList<>();
+                    for(ModInfo loadedMI : Loader.MODINFOS) {
+                        try {
+                            newModList.add(new File(loadedMI.jarURL.toURI()));
+                            //Add expansion right after main mod
+                            if(loadedMI.ID.equals(SpireAnniversary5Mod.modID)) {
+                                newModList.add(new File(m.jarURL.toURI()));
+                            }
+                        } catch (URISyntaxException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    ModList.save(ModList.getDefaultList(), newModList.toArray(new File[0]));
+                    break;
+                }
+            }
+            if(!found) {
+                SpireAnniversary5Mod.logger.error("Could not find expansion packs??? Please open a bug report on the steam workshop.");
+            } else {
+                //show that it's enabled remind them to restart and remove this button
+            }
         }
     }
 
