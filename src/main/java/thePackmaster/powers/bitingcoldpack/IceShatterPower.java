e8ea04480eedaeab6821793d33c344665f1faa3b
erasels <emanuelbedburdick@yahoo.de>
2023-07-11 21:00:55 +0200

    IceBite Pack: Fix Ice Shatter not triggering at the correct time

--- src/main/java/thePackmaster/powers/bitingcoldpack/IceShatterPower.java
+++ src/main/java/thePackmaster/powers/bitingcoldpack/IceShatterPower.java
@@ -1,7 +1,6 @@
 package thePackmaster.powers.bitingcoldpack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -13,41 +12,42 @@ import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 import static thePackmaster.util.Wiz.atb;
-import static thePackmaster.util.Wiz.att;
 
 public class IceShatterPower extends AbstractPackmasterPower {
     public static final String POWER_ID = makeID("IceShatterPower");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String[] DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
-    public boolean notActivatedForThisHPLoss;
+    public boolean activated;
 
     public IceShatterPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, AbstractPower.PowerType.DEBUFF, false, owner, amount);
-        notActivatedForThisHPLoss = true;
+    }
+
+    @Override
+    public void onInitialApplication() {
+        activated = false;
     }
 
     public void wasHPLost(DamageInfo info, int damageAmount) {
-        if (damageAmount > 0 && AbstractDungeon.actionManager.turnHasEnded && notActivatedForThisHPLoss) {
-            notActivatedForThisHPLoss = false;
-            att(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    notActivatedForThisHPLoss = true;
-                    this.isDone = true;
-                }
-            });
-            att(new DamageAction(this.owner, new DamageInfo(Wiz.p(), this.amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-            this.flash();
+        if (!activated) return;
+
+        if (damageAmount > 0 && AbstractDungeon.actionManager.turnHasEnded) {
+            Wiz.doDmg(owner, amount, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.BLUNT_HEAVY, false, true);
+            flash();
         }
     }
 
     @Override
     public void atEndOfRound() {
-        atb(new RemoveSpecificPowerAction(owner, owner, this));
+        if (!activated) {
+            activated = true;
+        } else {
+            atb(new RemoveSpecificPowerAction(owner, owner, this));
+        }
     }
 
     @Override
     public void updateDescription() {
-            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 }
