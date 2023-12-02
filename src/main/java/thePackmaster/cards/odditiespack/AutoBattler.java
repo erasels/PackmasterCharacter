cd43c6de7a4b5445e8e0605c54d14803199f7c23
erasels <emanuelbedburdick@yahoo.de>
2023-12-02 10:11:56 +0100

    Make AutoBattler gain focus

--- src/main/java/thePackmaster/cards/odditiespack/AutoBattler.java
+++ src/main/java/thePackmaster/cards/odditiespack/AutoBattler.java
@@ -3,8 +3,8 @@ package thePackmaster.cards.odditiespack;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.FocusPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
-import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.powers.odditiespack.AutoBattlerPower;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -17,15 +17,18 @@ public class AutoBattler extends AbstractOdditiesCard {
     public AutoBattler() {
         super(ID, 1, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
         baseMagicNumber = magicNumber = 5;
+        baseSecondMagic = secondMagic = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         applyToSelf(new StrengthPower(p, magicNumber));
         applyToSelf(new DexterityPower(p, magicNumber));
+        applyToSelf(new FocusPower(p, secondMagic));
         applyToSelf(new AutoBattlerPower());
     }
 
     public void upp() {
         upgradeMagicNumber(1);
+        upgradeSecondMagic(1);
     }
 }
