bc8eb0a9c91fac7d0d9783c3434b12a85cbdb70b
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2023-12-02 09:23:11 +0100

    Weapons Pack Rework

--- src/main/java/thePackmaster/powers/weaponspack/ArmorUpPower.java
+++ src/main/java/thePackmaster/powers/weaponspack/ArmorUpPower.java
@@ -1,7 +1,8 @@
 package thePackmaster.powers.weaponspack;
 
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
@@ -20,12 +21,11 @@ public class ArmorUpPower extends AbstractPackmasterPower {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
     }
 
-    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
-        if (damageAmount> 0 && target != this.owner && info.type == DamageInfo.DamageType.NORMAL && damageAmount + 3 >= info.base) {
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.type == AbstractCard.CardType.ATTACK && card.baseDamage != -1 && card.damage >= card.baseDamage + 3) {
             this.flash();
             this.addToBot(new GainBlockAction(this.owner, this.owner, this.amount));
         }
-
     }
 
     @Override
