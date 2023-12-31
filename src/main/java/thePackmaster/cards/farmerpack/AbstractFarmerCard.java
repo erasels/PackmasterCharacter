7c2e601b5c434773f45b75d4ec529139b20d8422
modargo <modargo@gmail.com>
2023-12-31 03:33:16 -0500

    Farmer pack: respect one frame for all config option

--- src/main/java/thePackmaster/cards/farmerpack/AbstractFarmerCard.java
+++ src/main/java/thePackmaster/cards/farmerpack/AbstractFarmerCard.java
@@ -3,6 +3,7 @@ package thePackmaster.cards.farmerpack;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.ThePackmaster;
 import thePackmaster.cards.AbstractPackmasterCard;
 
@@ -12,10 +13,12 @@ import java.util.Locale;
 public abstract class AbstractFarmerCard extends AbstractPackmasterCard {
     public AbstractFarmerCard(String cardID, int cost, CardType type, CardRarity rarity, CardTarget target, CardColor color) {
         super(cardID, cost, type, rarity, target, color);
-        setBackgroundTexture(
-                "anniv5Resources/images/512/farmer/" + type.name().toLowerCase(Locale.ROOT) + ".png",
-                "anniv5Resources/images/1024/farmer/" + type.name().toLowerCase(Locale.ROOT) + ".png"
-        );
+        if (!SpireAnniversary5Mod.oneFrameMode) {
+            setBackgroundTexture(
+                    "anniv5Resources/images/512/farmer/" + type.name().toLowerCase(Locale.ROOT) + ".png",
+                    "anniv5Resources/images/1024/farmer/" + type.name().toLowerCase(Locale.ROOT) + ".png"
+            );
+        }
     }
 
     public int checkTypes(boolean cardIsBeingPlayed){
