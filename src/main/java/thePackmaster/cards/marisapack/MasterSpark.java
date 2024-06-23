e5a727a77e076009fb5e3d12eb43514188a38436
erasels <emanuelbedburdick@yahoo.de>
2024-06-23 21:09:51 +0200

    MasterSpark: Fix damage calculation bug

--- src/main/java/thePackmaster/cards/marisapack/MasterSpark.java
+++ src/main/java/thePackmaster/cards/marisapack/MasterSpark.java
@@ -8,35 +8,65 @@ import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
-public class MasterSpark extends AbstractMarisaCard implements AmplifyCard{
+public class MasterSpark extends AbstractMarisaCard implements AmplifyCard {
     public final static String ID = makeID(MasterSpark.class.getSimpleName());
     private static final int DMG = 8, UPG_DMG = 2;
-    private static final int MAGIC = 17, UPG_MAGIC = 6;
+    private static final int MAGIC = 17 - DMG, UPG_MAGIC = 23 - (DMG + UPG_DMG);
 
     public MasterSpark() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
         damage = baseDamage = DMG;
-        baseSecondDamage = secondDamage = MAGIC;
+        baseMagicNumber = magicNumber = DMG + MAGIC;
         isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.vfx(new MindblastEffect(p.hb.cX, p.hb.cY, p.flipHorizontal), 0.1F);
         int tmpBaseDamage = baseDamage;
-        if(isAmplified(this)) {
-            baseDamage = baseSecondDamage;
-            applyPowers();
+        if (isAmplified(this)) {
+            baseDamage = baseMagicNumber;
+            calculateCardDamage(null);
         }
         allDmg(AbstractGameAction.AttackEffect.NONE);
-        if(isAmplified(this)) {
+        if (isAmplified(this)) {
             baseDamage = tmpBaseDamage;
             applyPowers();
         }
     }
 
+    // lots of jank here because using secondDamage will make the multiDamage array not upgrade
+    @Override
+    public void applyPowers() {
+        int tmp = baseDamage;
+        baseDamage = baseMagicNumber;
+
+        super.applyPowers();
+
+        magicNumber = damage;
+        baseDamage = tmp;
+
+        super.applyPowers();
+
+        isMagicNumberModified = (magicNumber != baseMagicNumber);
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int tmp = baseDamage;
+        baseDamage = baseMagicNumber;
+
+        super.calculateCardDamage(mo);
+
+        magicNumber = damage;
+        baseDamage = tmp;
+
+        super.calculateCardDamage(mo);
+
+        isMagicNumberModified = (magicNumber != baseMagicNumber);
+    }
+
     public void upp() {
         upgradeDamage(UPG_DMG);
-        upgradeSecondDamage(UPG_MAGIC);
     }
 
     @Override
