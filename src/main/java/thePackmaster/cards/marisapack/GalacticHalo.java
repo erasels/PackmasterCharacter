aed7dbedc70ccd2b0de456fd254a501f48027547
erasels <emanuelbedburdick@yahoo.de>
2024-06-23 15:35:33 +0200

    Rework Galactic halo

--- src/main/java/thePackmaster/cards/marisapack/GalacticHalo.java
+++ src/main/java/thePackmaster/cards/marisapack/GalacticHalo.java
@@ -4,19 +4,20 @@ import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.DarkOrbActivateEffect;
 import thePackmaster.powers.marisapack.ChargeUpPower;
 import thePackmaster.util.Wiz;
 import thePackmaster.vfx.marisapack.BubbleEffect;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
-public class GalacticHalo extends AbstractMarisaCard {
+public class GalacticHalo extends AbstractMarisaCard implements AmplifyCard {
     public final static String ID = makeID(GalacticHalo.class.getSimpleName());
-    private static final int BLK = 10, BLK_UPG = 3;
+    private static final int BLK = 12, BLK_UPG = 3;
     private static final int MAGIC = 3;
 
     public GalacticHalo() {
-        super(ID, 2, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         baseBlock = block = BLK;
         baseMagicNumber = magicNumber = MAGIC;
 
@@ -24,11 +25,42 @@ public class GalacticHalo extends AbstractMarisaCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.vfx(new BubbleEffect(Color.SKY, ""), Settings.ACTION_DUR_FAST);
-        blck();
         Wiz.applyToSelf(new ChargeUpPower(magicNumber));
     }
 
     public void upp() {
        upgradeBlock(BLK_UPG);
+    }
+
+    @Override
+    public boolean skipUseOnAmplify() {
+        return false;
+    }
+
+    @Override
+    public void useAmplified(AbstractPlayer p, AbstractMonster m) {
+        queueVFXAroundPlayer();
+        blck();
+    }
+
+    @Override
+    public int getAmplifyCost() {
+        return 2;
+    }
+
+    public void queueVFXAroundPlayer() {
+        float playerX = Wiz.p().hb.cX;
+        float playerY = Wiz.p().hb.cY;
+
+        float distance = 20 * Settings.scale;
+
+        // Angles in radians
+        float[] angles = {0, (float)Math.PI / 2, (float)Math.PI, 3 * (float)Math.PI / 2};
+
+        for (float angle : angles) {
+            float vfxX = playerX + distance * (float)Math.cos(angle);
+            float vfxY = playerY + distance * (float)Math.sin(angle);
+            Wiz.vfx(new DarkOrbActivateEffect(vfxX, vfxY));
+        }
     }
 }
