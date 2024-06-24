c5c208427e473a466e69bbf632223f759a27259d
erasels <emanuelbedburdick@yahoo.de>
2024-06-24 13:28:31 +0200

    Blazing star: Add vfx

--- src/main/java/thePackmaster/cards/marisapack/BlazingStar.java
+++ src/main/java/thePackmaster/cards/marisapack/BlazingStar.java
@@ -1,9 +1,13 @@
 package thePackmaster.cards.marisapack;
 
+import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.FlameBarrierEffect;
 import thePackmaster.powers.marisapack.FreeAmplifyPower;
 import thePackmaster.util.Wiz;
 
@@ -19,6 +23,10 @@ public class BlazingStar  extends AbstractMarisaCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractGameEffect vfx = new FlameBarrierEffect(p.hb.cX, p.hb.cY);
+        ReflectionHacks.setPrivate(vfx, FlameBarrierEffect.class, "flashedBorder", false);
+        Wiz.vfx(vfx, Settings.ACTION_DUR_XFAST);
+
         Wiz.applyToSelf(new StrengthPower(p, magicNumber));
         Wiz.applyToSelf(new LoseStrengthPower(p, magicNumber));
 
