1be16983d94f093f86f8a5422b9b0c65dc070af2
erasels <emanuelbedburdick@yahoo.de>
2024-06-25 22:24:23 +0200

    Luminous Strike: Only play vfx if ignite would be applied

--- src/main/java/thePackmaster/cards/marisapack/LuminousStrike.java
+++ src/main/java/thePackmaster/cards/marisapack/LuminousStrike.java
@@ -26,7 +26,7 @@ public class LuminousStrike extends AbstractMarisaCard implements AmplifyCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        if(damage > 0) {
+        if (damage > 0) {
             Wiz.atb(new AbstractGameAction() {
                 @Override
                 public void update() {
@@ -50,17 +50,17 @@ public class LuminousStrike extends AbstractMarisaCard implements AmplifyCard {
 
     @Override
     public void useAmplified(AbstractPlayer p, AbstractMonster m) {
-        Wiz.vfx(new FireIgniteEffect(m.hb.cX, m.hb.cY, damage));
-        Wiz.atb(new AbstractGameAction() {
-            @Override
-            public void update() {
-                isDone = true;
-                for (int i = 0; i < 4; i++) {
-                    AbstractDungeon.effectList.add(new CasualFlameParticleEffect(m.hb.cX + (MathUtils.random(-50, 50) * Settings.scale), m.hb.cY + (MathUtils.random(-50, 50) * Settings.scale)));
-                }
-            }
-        });
         if (damage > 1) { // 1 damage would apply 0
+            Wiz.vfx(new FireIgniteEffect(m.hb.cX, m.hb.cY, damage));
+            Wiz.atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    isDone = true;
+                    for (int i = 0; i < 4; i++) {
+                        AbstractDungeon.effectList.add(new CasualFlameParticleEffect(m.hb.cX + (MathUtils.random(-50, 50) * Settings.scale), m.hb.cY + (MathUtils.random(-50, 50) * Settings.scale)));
+                    }
+                }
+            });
             Wiz.applyToEnemy(m, new IgnitePower(m, damage / 2));
         }
     }
