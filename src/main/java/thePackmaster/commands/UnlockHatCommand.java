fc60fbf91c3a4a65442bbf9b955ab4356a59df82
erasels <emanuelbedburdick@yahoo.de>
2024-06-26 10:37:21 +0200

    UnlockHatCommand: simplify and add all option

--- src/main/java/thePackmaster/commands/UnlockHatCommand.java
+++ src/main/java/thePackmaster/commands/UnlockHatCommand.java
@@ -20,41 +20,68 @@ public class UnlockHatCommand extends ConsoleCommand {
             DevConsole.log("You need to add the name of the pack of the hat you want.");
             return;
         }
+
+        ArrayList<String> unlockedHats = SpireAnniversary5Mod.getUnlockedHats();
+        ArrayList<String> unseenHats = SpireAnniversary5Mod.getUnseenHats();
+
+        if (tokens[1].equalsIgnoreCase("all")) {
+            boolean addedHats = false;
+            for (AbstractCardPack pack : SpireAnniversary5Mod.unfilteredAllPacks) {
+                addedHats = addedHats || addHatIfNotPresent(pack.packID, unlockedHats, unseenHats);
+            }
+            if (addedHats) {
+                saveHats(unlockedHats, unseenHats);
+            }
+            return;
+        }
+
         boolean found = false;
         String id = "";
-        for(AbstractCardPack pack : SpireAnniversary5Mod.unfilteredAllPacks) {
-            if(pack.name.equals(tokens[1].replace("_", " "))) {
+        for (AbstractCardPack pack : SpireAnniversary5Mod.unfilteredAllPacks) {
+            if (pack.name.equals(tokens[1].replace("_", " "))) {
                 found = true;
                 id = pack.packID;
                 break;
             }
         }
 
-        if(found) {
-            ArrayList<String> unlockedHats = SpireAnniversary5Mod.getUnlockedHats();
-            ArrayList<String> unseenHats = SpireAnniversary5Mod.getUnseenHats();
-            if (!unlockedHats.contains(id)) {
-                try {
-                    unlockedHats.add(id);
-                    unseenHats.add(id);
-                    SpireAnniversary5Mod.logger.info("Saving hat " + id + "|" + tokens[1] + ", acquired via console.");
-                    SpireAnniversary5Mod.saveUnlockedHats(unlockedHats);
-                    SpireAnniversary5Mod.saveUnseenHats(unseenHats);
-                    HatMenu.refreshHatDropdown();
-                } catch (Exception e) {
-                    SpireAnniversary5Mod.logger.error(e);
-                }
-            } else {
-                DevConsole.log("Hat is already unlocked.");
+        if (found) {
+            if (addHatIfNotPresent(id, unlockedHats, unseenHats)) {
+                saveHats(unlockedHats, unseenHats);
             }
         } else {
             errorMsg();
         }
     }
 
+    private boolean addHatIfNotPresent(String hatId, ArrayList<String> unlockedHats, ArrayList<String> unseenHats) {
+        boolean hasHat = unlockedHats.contains(hatId);
+        if (!hasHat) {
+            try {
+                unlockedHats.add(hatId);
+                unseenHats.add(hatId);
+                SpireAnniversary5Mod.logger.info("Saving hat " + hatId + ", acquired via console.");
+            } catch (Exception e) {
+                SpireAnniversary5Mod.logger.error(e);
+            }
+        }
+        return !hasHat;
+    }
+
+    private void saveHats(ArrayList<String> unlockedHats, ArrayList<String> unseenHats) {
+        try {
+            SpireAnniversary5Mod.saveUnlockedHats(unlockedHats);
+            SpireAnniversary5Mod.saveUnseenHats(unseenHats);
+            HatMenu.refreshHatDropdown();
+        } catch (Exception e) {
+            SpireAnniversary5Mod.logger.error(e);
+        }
+    }
+
     @Override
     public ArrayList<String> extraOptions(String[] tokens, int depth) {
         ArrayList<String> result = new ArrayList<>();
+        result.add("all");
         ArrayList<String> unlockedHats = SpireAnniversary5Mod.getUnlockedHats();
         SpireAnniversary5Mod.unfilteredAllPacks.stream()
                 .filter(p -> !unlockedHats.contains(p.packID))
