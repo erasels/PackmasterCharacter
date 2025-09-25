ac6071deae252cdacac1892a8d081fef39bbe24b
erasels <emanuelbedburdick@yahoo.de>
2025-09-25 21:47:32 +0200

    feat(Quiet): Add Weight preview to Tarred Feather relic

--- src/main/java/thePackmaster/relics/quietpack/TarredFeather.java
+++ src/main/java/thePackmaster/relics/quietpack/TarredFeather.java
@@ -1,8 +1,10 @@
 package thePackmaster.relics.quietpack;
 
 import basemod.BaseMod;
+import basemod.helpers.CardPowerTip;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import thePackmaster.SpireAnniversary5Mod;
 import thePackmaster.cards.quietpack.Weight;
@@ -10,10 +12,14 @@ import thePackmaster.packs.QuietPack;
 import thePackmaster.relics.AbstractPackmasterRelic;
 
 public class TarredFeather extends AbstractPackmasterRelic {
+    private static final AbstractCard PREVIEW = new Weight();
     public static final String ID = SpireAnniversary5Mod.makeID("TarredFeather");
 
     public TarredFeather() {
         super(ID, RelicTier.SHOP, LandingSound.FLAT, QuietPack.ID);
+        if(tips.stream().noneMatch(t->t instanceof CardPowerTip)) {
+            tips.add(new CardPowerTip(PREVIEW));
+        }
     }
 
     @Override
